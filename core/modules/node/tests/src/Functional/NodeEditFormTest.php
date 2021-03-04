5b9dabdad72feb03c6d69259a5fd3c124e541324
SUDIPTA DAS <dsudipta2012@gmail.com>
2021-03-04 16:47:03 +0530

    added test for showing node author display name

--- core/modules/node/tests/src/Functional/NodeEditFormTest.php
+++ core/modules/node/tests/src/Functional/NodeEditFormTest.php
@@ -304,4 +304,24 @@ class NodeEditFormTest extends NodeTestBase {
     $this->assertSame($this->webUser->id(), $node->getOwnerId(), 'Node authored by normal user.');
   }
 
+  /**
+   * Test that author display name is visible on node edit form.
+   */
+  public function testNodeAuthorDisplayName() {
+    $this->drupalLogin($this->adminUser);
+    $this->drupalGet('node/add/page');
+    // Create node to edit.
+    $edit['title[0][value]'] = $this->randomMachineName(8);
+    $edit['body[0][value]'] = $this->randomMachineName(16);
+    $this->submitForm($edit, 'Save');
+    // Check that the default value in user name field
+    // is the raw value and not a formatted one.
+    \Drupal::service('module_installer')->install(['user_hooks_test']);
+    Cache::invalidateTags(['rendered']);
+    \Drupal::state()->set('user_hooks_test_user_format_name_alter', TRUE);
+    $node = $this->drupalGetNodeByTitle($edit['title[0][value]']);
+    $this->drupalGet("node/" . $node->id() . "/edit");
+    $this->assertSession()->responseContains('<em>' . $this->adminUser->id() . '</em>');
+  }
+
 }
