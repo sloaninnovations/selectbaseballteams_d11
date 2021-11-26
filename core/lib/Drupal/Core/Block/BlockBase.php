53ed772f200d385bef011ab8d6a48e222b99e7ea
Rob Mumford <rlmumford@googlemail.com>
2021-11-26 15:33:24 +0000

    Handle case where there is no context mapping set.

--- core/lib/Drupal/Core/Block/BlockBase.php
+++ core/lib/Drupal/Core/Block/BlockBase.php
@@ -46,7 +46,7 @@ abstract class BlockBase extends PluginBase implements BlockPluginInterface, Plu
    * {@inheritdoc}
    */
   public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
-    if (!$form_state->getErrors()) {
+    if (!$form_state->getErrors() && $form_state->getValue('context_mapping')) {
       $this->configuration['context_mapping'] = $form_state->getValue('context_mapping');
     }
     $this->traitSubmitConfigurationForm($form, $form_state);
