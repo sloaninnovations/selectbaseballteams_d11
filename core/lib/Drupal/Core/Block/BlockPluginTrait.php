5ca0a62aca49da3c1dbc2f6706c1d79fc3836e20
Rob Mumford <rlmumford@googlemail.com>
2021-11-26 15:50:39 +0000

    Don't set the mapping when there is no mapping to set.

--- core/lib/Drupal/Core/Block/BlockPluginTrait.php
+++ core/lib/Drupal/Core/Block/BlockPluginTrait.php
@@ -227,7 +227,9 @@ trait BlockPluginTrait {
       $this->configuration['label'] = $form_state->getValue('label');
       $this->configuration['label_display'] = $form_state->getValue('label_display');
       $this->configuration['provider'] = $form_state->getValue('provider');
-      $this->configuration['context_mapping'] = $form_state->getValue('context_mapping');
+      if ($form_state->getValue('context_mapping')) {
+        $this->configuration['context_mapping'] = $form_state->getValue('context_mapping');
+      }
       $this->blockSubmit($form, $form_state);
     }
   }
