9084b7fa2ee0d11e582cf9162fc99411150fb6c3
Claudiu Cristea <clau.cristea@gmail.com>
2022-01-31 10:10:15 +0200

    Fix quote duplication

--- core/modules/comment/src/CommentFieldItemList.php
+++ core/modules/comment/src/CommentFieldItemList.php
@@ -54,7 +54,7 @@ class CommentFieldItemList extends FieldItemList {
     }
     if ($operation === 'view') {
       // This operation is only used by EntityViewDisplay::buildMultiple(). In
-      // this case check both: 'view comment list'' or 'create' access, since
+      // this case check both: 'view comment list' or 'create' access, since
       // this field is considered a composition of listed comments and comment
       // form. The decision to show only comments, only comment form or both
       // will be made by CommentDefaultFormatter::viewElements() later. Uses
