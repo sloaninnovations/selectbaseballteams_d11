ccc8ddda2e40b73297896abb542689c1e45f78ff
Bhanu951 <Bhanu951@3607219.no-reply.drupal.org>
2023-02-07 14:48:33 +0530

    Issue #2835545: Provide a Workflow FieldType that references a workflow entity and tracks the current state of an entity

---
+++ core/modules/workflows/src/WorkflowStateFieldItemList.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Drupal\workflows;
+
+use Drupal\Core\Entity\FieldableEntityInterface;
+use Drupal\Core\Field\FieldDefinitionInterface;
+use Drupal\Core\Field\FieldItemList;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\workflows\Entity\Workflow;
+
+/**
+ * Defines an item list class for the 'workflow_state' field type.
+ */
+class WorkflowStateFieldItemList extends FieldItemList {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function processDefaultValue($default_value, FieldableEntityInterface $entity, FieldDefinitionInterface $definition) {
+    // The default value of a workflow state field can only be the initial state
+    // of the workflow.
+    $storage_definition = $definition->getFieldStorageDefinition();
+    if ($callback = $storage_definition->getSetting('workflow_callback')) {
+      $workflow_id = call_user_func($callback, $entity);
+    }
+    else {
+      $workflow_id = $storage_definition->getSetting('workflow');
+    }
+
+    /** @var \Drupal\workflows\WorkflowInterface $workflow */
+    $workflow = Workflow::load($workflow_id);
+    return [$workflow->getTypePlugin()->getInitialState()->id()];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultValuesForm(array &$form, FormStateInterface $form_state) {
+    $workflow = $this->list[0]->getWorkflow();
+    return [
+      '#markup' => $workflow->getTypePlugin()->getState($this->list[0]->value)->label(),
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function defaultValueWidget(FormStateInterface $form_state) {
+    return NULL;
+  }
+
+}
--- core/modules/workflows/workflows.api.php
+++ core/modules/workflows/workflows.api.php
@@ -16,3 +16,57 @@
  * Content Moderation to give the workflow context.
  * @}
  */
+
+/**
+ * @defgroup workflow_transition_hooks Workflow Transition Hooks
+ * @{
+ * For workflows that are attached to a fieldable entity type by using the
+ * 'workflow_state' field type, two transition hooks are invoked, allowing
+ * modules to react before and after the transition has been performed.
+ * @}
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
+
+/**
+ * Acts before a workflow transition is performed.
+ *
+ * This hook is invoked before the new state is written to the storage.
+ *
+ * @param \Drupal\workflows\WorkflowInterface $workflow
+ *   The workflow.
+ * @param \Drupal\workflows\Transition $transition
+ *   The transition.
+ * @param \Drupal\Core\Entity\FieldableEntityInterface $entity
+ *   The entity for which a workflow state is changed.
+ * @param string $field_name
+ *   THe name of the field to which the workflow is attached.
+ */
+function hook_workflows_pre_transition(\Drupal\workflows\WorkflowInterface $workflow, \Drupal\workflows\Transition $transition, \Drupal\Core\Entity\FieldableEntityInterface $entity, $field_name) {
+  // @todo
+}
+
+/**
+ * Acts after a workflow transition is performed.
+ *
+ * This hook is invoked after the new state is written to the storage.
+ *
+ * @param \Drupal\workflows\WorkflowInterface $workflow
+ *   The workflow.
+ * @param \Drupal\workflows\Transition $transition
+ *   The transition.
+ * @param \Drupal\Core\Entity\FieldableEntityInterface $entity
+ *   The entity for which a workflow state is changed.
+ * @param string $field_name
+ *   THe name of the field to which the workflow is attached.
+ */
+function hook_workflows_post_transition(\Drupal\workflows\WorkflowInterface $workflow, \Drupal\workflows\Transition $transition, \Drupal\Core\Entity\FieldableEntityInterface $entity, $field_name) {
+  // @todo
+}
+
+/**
+ * @} End of "addtogroup hooks".
+ */
