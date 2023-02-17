e5351557e736f4cf8e5e6f2be674582df6bc5301
Roman Paska <roman.paska@lembergsolutions.com>
2023-02-17 23:30:39 +0200

    Evertyhing should be OK now

--- core/lib/Drupal/Core/Asset/AssetResolver.php
+++ core/lib/Drupal/Core/Asset/AssetResolver.php
@@ -181,6 +181,9 @@ class AssetResolver implements AssetResolverInterface {
       [$extension, $name] = explode('/', $library, 2);
       $definition = $this->libraryDiscovery->getLibraryByName($extension, $name);
       foreach ($definition['css'] as $options) {
+        if (array_key_exists($options['data'], $css)) {
+          continue;
+        }
         $options += $default_options;
         // Copy the asset library license information to each file.
         $options['license'] = $definition['license'];
@@ -189,10 +192,6 @@ class AssetResolver implements AssetResolverInterface {
         if ($options['type'] === 'file' && $options['preprocess'] && str_contains($options['data'], '?')) {
           $options['preprocess'] = FALSE;
         }
-
-        // Always add a tiny value to the weight, to conserve the insertion
-        // order.
-        $options['weight'] += count($css) / 30000;
 
         // CSS files are being keyed by the full path.
         $css[$options['data']] = $options;
