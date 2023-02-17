b347cb5bd1cf7b073405aa3c2c5e5f0d748ec813
Alfonso Gómez-Arzola <alfonso@chromatichq.com>
2023-02-16 21:34:17 -0500

    Update fragment link event listener selector to avoid links inside WYSIWYG editors

--- core/misc/form.js
+++ core/misc/form.js
@@ -319,7 +319,8 @@
    */
   $(document).on(
     'click.form-fragment',
-    'a[href*="#"]',
+    // Target all links except those inside a WYSIWYG editor.
+    'a[href*="#"]:not(.form-textarea-wrapper *)',
     debouncedHandleFragmentLinkClickOrHashChange,
   );
 })(jQuery, Drupal, Drupal.debounce);
