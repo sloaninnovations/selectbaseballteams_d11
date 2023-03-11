ad69ef63156af49f77ea2dbc51a03f5ce6cc232e
Alfonso Gómez-Arzola <alfonso@chromatichq.com>
2023-03-10 19:59:41 -0500

    Add JS form test

---
+++ core/tests/Drupal/Nightwatch/Tests/jsFormTest.js
@@ -0,0 +1,59 @@
+module.exports = {
+  '@tags': ['core'],
+  before(browser) {
+    browser.drupalInstall();
+  },
+  after(browser) {
+    browser.drupalUninstall();
+  },
+  'Test clicking anchor in WYSIWYG editor': (browser) => {
+    const mainContentSelector = '#main-content';
+    const wysiwygSelector = '.ck-content[contenteditable="true"]';
+    const testAnchorSelector = 'a[href="#main-content"]';
+    let mainContentTopPosition;
+    browser.drupalLoginAsAdmin(() => {
+      browser.drupalRelativeURL('/node/add/page');
+      // Scroll down past the #main-content element and store the top position
+      // of #main-content (in relation to the viewport) in a variable.
+      mainContentTopPosition = browser.execute(
+        function scrollToTargetAndGetTopPosition(mSelector, wSelector) {
+          // The WYSIWYG editor is below the #main-content element, so we
+          // scroll down to it. This ensures that #main-content is outside the
+          // bounds of the viewport.
+          wSelector.scrollIntoView();
+          const mainContent = document.querySelector(mSelector);
+          const rect = mainContent.getBoundingClientRect();
+          return rect.top;
+        },
+        [mainContentSelector, wysiwygSelector],
+      );
+      // Focus on CKEditor WYSIWYG editor and type some text into it.
+      browser
+        .click(wysiwygSelector)
+        .keys('Back to top'.split(''))
+        // Select the text and press Ctrl+k to open up the link creation UI.
+        .keys([browser.Keys.CONTROL, 'a'])
+        .keys([browser.Keys.CONTROL, 'k'])
+        // Enter #main-content into the href and press Return key to submit the
+        // link creation form.
+        .keys(mainContentSelector.split(''))
+        .keys([browser.Keys.RETURN])
+        // Wait for the link to appear in the WYSIWYG and click on it.
+        .waitForElementVisible(testAnchorSelector, 1000)
+        .click(testAnchorSelector);
+      // Assert that the position of the top of #main-content is intact.
+      const newMainContentTopPosition = browser.execute(
+        function getTopPosition(mSelector) {
+          const mainContent = document.querySelector(mSelector);
+          const rect = mainContent.getBoundingClientRect();
+          return rect.top;
+        },
+        [mainContentSelector],
+      );
+      browser.assert.ok(
+        mainContentTopPosition === newMainContentTopPosition,
+        'Clicking anchor link inside WYSIWYG caused browser to navigate to target.',
+      );
+    });
+  },
+};
