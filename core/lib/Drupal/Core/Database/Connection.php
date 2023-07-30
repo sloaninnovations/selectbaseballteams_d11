a6d44aff1d0a4940e46ca8529effb10fe0217d9c
mondrake <mondrake.org@gmail.com>
2023-07-30 20:31:24 +0200

    temp fix

--- core/lib/Drupal/Core/Database/Connection.php
+++ core/lib/Drupal/Core/Database/Connection.php
@@ -352,7 +352,7 @@ abstract class Connection {
    * @return string $prefix
    */
   public function getPrefix(): string {
-    @trigger_error(__METHOD__ . '() is deprecated in drupal:9.x.0 and is removed from drupal:10.0.0. @todo. See https://www.drupal.org/node/1234567', E_USER_DEPRECATED);
+    // @trigger_error(__METHOD__ . '() is deprecated in drupal:9.x.0 and is removed from drupal:10.0.0. @todo. See https://www.drupal.org/node/1234567', E_USER_DEPRECATED);
     return $this->identifierHandler->getTablePrefix();
   }
 
