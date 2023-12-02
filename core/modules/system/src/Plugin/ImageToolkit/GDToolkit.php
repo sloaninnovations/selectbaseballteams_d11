f297009a4915e217ac1bfab3c853a3b5171d9373
mondrake <mondrake.org@gmail.com>
2023-12-02 15:35:32 +0100

    Add caching of the support check

--- core/modules/system/src/Plugin/ImageToolkit/GDToolkit.php
+++ core/modules/system/src/Plugin/ImageToolkit/GDToolkit.php
@@ -55,20 +55,6 @@ class GDToolkit extends ImageToolkitBase {
   protected $preLoadInfo = NULL;
 
   /**
-   * The StreamWrapper manager.
-   *
-   * @var \Drupal\Core\StreamWrapper\StreamWrapperManagerInterface
-   */
-  protected $streamWrapperManager;
-
-  /**
-   * The file system.
-   *
-   * @var \Drupal\Core\File\FileSystemInterface
-   */
-  protected $fileSystem;
-
-  /**
    * Constructs a GDToolkit object.
    *
    * @param array $configuration
@@ -83,15 +69,25 @@ class GDToolkit extends ImageToolkitBase {
    *   A logger instance.
    * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
    *   The config factory.
-   * @param \Drupal\Core\StreamWrapper\StreamWrapperManagerInterface $stream_wrapper_manager
+   * @param \Drupal\Core\StreamWrapper\StreamWrapperManagerInterface $streamWrapperManager
    *   The StreamWrapper manager.
-   * @param \Drupal\Core\File\FileSystemInterface $file_system
+   * @param \Drupal\Core\File\FileSystemInterface $fileSystem
    *   The file system.
+   * @param \Drupal\Core\Cache\CacheBackendInterface $cacheDefault
+   *   The default cache bin.
    */
-  public function __construct(array $configuration, $plugin_id, array $plugin_definition, ImageToolkitOperationManagerInterface $operation_manager, LoggerInterface $logger, ConfigFactoryInterface $config_factory, StreamWrapperManagerInterface $stream_wrapper_manager, FileSystemInterface $file_system) {
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    array $plugin_definition,
+    ImageToolkitOperationManagerInterface $operation_manager,
+    LoggerInterface $logger,
+    ConfigFactoryInterface $config_factory,
+    protected readonly StreamWrapperManagerInterface $streamWrapperManager,
+    protected readonly FileSystemInterface $fileSystem,
+    protected readonly CacheBackendInterface $cacheDefault,
+  ) {
     parent::__construct($configuration, $plugin_id, $plugin_definition, $operation_manager, $logger, $config_factory);
-    $this->streamWrapperManager = $stream_wrapper_manager;
-    $this->fileSystem = $file_system;
   }
 
   /**
@@ -106,7 +102,8 @@ class GDToolkit extends ImageToolkitBase {
       $container->get('logger.channel.image'),
       $container->get('config.factory'),
       $container->get('stream_wrapper_manager'),
-      $container->get('file_system')
+      $container->get('file_system'),
+      $container->get('cache.default'),
     );
   }
 
@@ -546,6 +543,11 @@ class GDToolkit extends ImageToolkitBase {
         '@unsupported' => $unsupported,
         '@ref' => $fix_info,
       ]);
+      if (isset($unsupported_formats[IMG_AVIF])) {
+        $descriptions[] = $this->t('AVIF is likely not supported because of PHP missing a codec for encoding images. See <a href=":cr_url">the change record</a> for more information.', [
+          ':cr_url' => 'https://www.drupal.org/node/3348348',
+        ]);
+      }
     }
 
     // Check for filter and rotate support.
@@ -627,6 +629,10 @@ class GDToolkit extends ImageToolkitBase {
    *   TRUE if AVIF is fully supported.
    */
   protected function checkAvifSupport(): bool {
+    if ($cache = $this->cacheDefault->get('gdtoolkit_avif_support')) {
+      return $cache->data;
+    }
+
     $supported = TRUE;
     $previous_error_handler = set_error_handler(function ($severity, $message, $file, $line) use (&$previous_error_handler, &$supported) {
       if (str_starts_with($message, 'imageavif()')) {
@@ -636,7 +642,13 @@ class GDToolkit extends ImageToolkitBase {
         return $previous_error_handler($severity, $message, $file, $line);
       }
     });
-    imageavif(imagecreatetruecolor(1, 1), $this->fileSystem->tempnam('temporary://', 'avif'));
+
+    $tempFile = $this->fileSystem->tempnam('temporary://', 'avif');
+    imageavif(imagecreatetruecolor(1, 1), $this->fileSystem->tempnam($tempFile));
+    $this->fileSystem->unlink($tempFile);
+
+    $this->cacheDefault->set('gdtoolkit_avif_support', $supported);
+
     return $supported;
   }
 
