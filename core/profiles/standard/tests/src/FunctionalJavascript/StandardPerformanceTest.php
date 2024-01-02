cc3a5dbe0cf6440182290cf0acde2e51579fcee1
Adam Bramley <46227-acbramley@users.noreply.drupalcode.org>
2024-01-02 01:33:00 +0000

    Fix more counts

--- core/profiles/standard/tests/src/FunctionalJavascript/StandardPerformanceTest.php
+++ core/profiles/standard/tests/src/FunctionalJavascript/StandardPerformanceTest.php
@@ -82,7 +82,7 @@ class StandardPerformanceTest extends PerformanceTestBase {
     $this->assertLessThanOrEqual(39, $performance_data->getQueryCount());
     $this->assertGreaterThanOrEqual(85, $performance_data->getCacheGetCount());
     $this->assertLessThanOrEqual(88, $performance_data->getCacheGetCount());
-    $this->assertGreaterThanOrEqual(20, $performance_data->getCacheSetCount());
+    $this->assertGreaterThanOrEqual(18, $performance_data->getCacheSetCount());
     $this->assertLessThanOrEqual(28, $performance_data->getCacheSetCount());
     $this->assertSame(0, $performance_data->getCacheDeleteCount());
 
