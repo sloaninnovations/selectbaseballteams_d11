54d765b3c092dc672ebf1bb827184dba6102ee55
Siju Mathew <65250-sijumpk@users.noreply.drupalcode.org>
2024-01-16 07:51:41 +0000

    Update FunctionalTestSetupTrait.php; changing the second parameter type to mixed for setContainerParameter in phpdoc

--- core/lib/Drupal/Core/Test/FunctionalTestSetupTrait.php
+++ core/lib/Drupal/Core/Test/FunctionalTestSetupTrait.php
@@ -177,7 +177,7 @@ trait FunctionalTestSetupTrait {
    *
    * @param string $name
    *   The name of the parameter.
-   * @param string $value
+   * @param mixed $value
    *   The value of the parameter.
    */
   protected function setContainerParameter($name, $value) {
