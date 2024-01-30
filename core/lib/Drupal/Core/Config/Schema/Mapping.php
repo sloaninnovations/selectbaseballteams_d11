731eb007646d3f2a7c87cb078fae4a71f588fe33
Wim Leers <wim.leers@acquia.com>
2024-01-30 18:27:07 +0100

    Broaden return type again for `Sequence` and `Mapping`, because we have no other choice.

--- core/lib/Drupal/Core/Config/Schema/Mapping.php
+++ core/lib/Drupal/Core/Config/Schema/Mapping.php
@@ -297,7 +297,7 @@ class Mapping extends ArrayElement {
   /**
    * {@inheritdoc}
    */
-  public function getCanonicalRepresentation(bool $suppress_exceptions = FALSE): ?array {
+  public function getCanonicalRepresentation(bool $suppress_exceptions = FALSE): mixed {
     if ($this->value === NULL) {
       return NULL;
     }
--- core/lib/Drupal/Core/Config/Schema/Sequence.php
+++ core/lib/Drupal/Core/Config/Schema/Sequence.php
@@ -43,7 +43,7 @@ class Sequence extends ArrayElement {
   /**
    * {@inheritdoc}
    */
-  public function getCanonicalRepresentation(bool $suppress_exceptions = FALSE): ?array {
+  public function getCanonicalRepresentation(bool $suppress_exceptions = FALSE): mixed {
     if ($this->value === NULL) {
       return NULL;
     }
