873b191078438d11b6439c8bde612b597e0c70ae
Adam G-H <32250-phenaproxima@users.noreply.drupalcode.org>
2024-01-30 14:49:30 +0000

    Alternative solution by @phenaproxima for what @bircher raised.

--- core/lib/Drupal/Core/Config/Schema/Sequence.php
+++ core/lib/Drupal/Core/Config/Schema/Sequence.php
@@ -84,7 +84,7 @@ class Sequence extends ArrayElement {
       // configuration schema.
       'value' => sort($representation),
       // Nothing to do when `orderby === NULL`.
-      NULL => 'no-op',
+      default => 'no-op',
     };
 
     return $representation;
