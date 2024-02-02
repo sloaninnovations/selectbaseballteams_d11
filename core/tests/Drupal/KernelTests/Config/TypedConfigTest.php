6d4b99ba015433e276f2055b01ed8551efba3dad
Wim Leers <wim.leers@acquia.com>
2024-02-02 14:21:24 +0100

    `Adjust `TypedConfigTest::testTypedDataAPI()` expectation: `::getElements()` always returns keys in the raw order, but because this is looking at a _saved_ config entity, they are now correctly sorted.

--- core/tests/Drupal/KernelTests/Config/TypedConfigTest.php
+++ core/tests/Drupal/KernelTests/Config/TypedConfigTest.php
@@ -227,7 +227,7 @@ class TypedConfigTest extends KernelTestBase {
     $typed_config_manager = \Drupal::service('config.typed');
     $typed_config = $typed_config_manager->createFromNameAndData('config_test.validation', \Drupal::configFactory()->get('config_test.validation')->get());
     $this->assertInstanceOf(TypedConfigInterface::class, $typed_config);
-    $this->assertEquals(['_core', 'llama', 'cat', 'giraffes', 'zoo', 'uuid', 'langcode', 'string__not_blank'], array_keys($typed_config->getElements()));
+    $this->assertEquals(['_core', 'langcode', 'zoo', 'llama', 'cat', 'giraffes', 'uuid', 'string__not_blank'], array_keys($typed_config->getElements()));
     $this->assertSame('config_test.validation', $typed_config->getName());
     $this->assertSame('config_test.validation', $typed_config->getPropertyPath());
     $this->assertSame('config_test.validation.llama', $typed_config->get('llama')->getPropertyPath());
