0117a37a961d87a70f6038d648f5fdba64210008
Wim Leers <wim.leers@acquia.com>
2024-02-06 10:54:45 +0100

    Fix `InstallStorage` to allow config schemas to be found in VFS roots.

--- core/lib/Drupal/Core/Config/InstallStorage.php
+++ core/lib/Drupal/Core/Config/InstallStorage.php
@@ -248,7 +248,8 @@ class InstallStorage extends FileStorage {
    *   The configuration folder name for this component.
    */
   protected function getComponentFolder(Extension $extension) {
-    return $extension->getPath() . '/' . $this->getCollectionDirectory();
+    // Prefix with the root to allow a VFS-based root in kernel tests.
+    return \Drupal::root() . '/' . $extension->getPath() . '/' . $this->getCollectionDirectory();
   }
 
   /**
