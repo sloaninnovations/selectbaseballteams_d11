957585c920e2c288a3a15f39830b82455a61d1c3
quietone <quietone@2572884.no-reply.drupal.org>
2024-02-09 19:52:47 +1300

    fulldate

--- core/modules/comment/src/CommentViewsData.php
+++ core/modules/comment/src/CommentViewsData.php
@@ -5,7 +5,7 @@ namespace Drupal\comment;
 use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\views\EntityViewsData;
 
-// cspell:ignore fulldata
+// cspell:ignore fulldate
 
 /**
  * Provides views data for the comment entity type.
