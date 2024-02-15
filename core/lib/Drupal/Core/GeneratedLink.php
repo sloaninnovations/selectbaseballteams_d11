84091f6f4a44c0baecae5b29db1e35347b3eb8fe
GafgarionMorua <cesar_morua_1@hotmail.com>
2024-02-15 09:54:07 -0600

    Issue #3392740: Reverted changes made to comments not relevant to this issue

--- core/lib/Drupal/Core/GeneratedLink.php
+++ core/lib/Drupal/Core/GeneratedLink.php
@@ -6,11 +6,11 @@ use Drupal\Component\Render\MarkupInterface;
 use Drupal\Core\Render\BubbleableMetadata;
 
 /**
- * Used to return generated links and their related cacheability data.
+ * Used to return generated links, along with associated cacheability metadata.
  *
- * Note: Don't confuse this with \Drupal\Core\Link. That's used for dealing
- *  with links that are not yet generated (typically, these can be a combination of
- *  link text, route name, and route parameters).
+ * Note: not to be confused with \Drupal\Core\Link, which is for passing around
+ *    links that are not generated (typically link text + route name + route
+ *    parameters).
  */
 class GeneratedLink extends BubbleableMetadata implements MarkupInterface, \Countable {
 
--- core/lib/Drupal/Core/GeneratedUrl.php
+++ core/lib/Drupal/Core/GeneratedUrl.php
@@ -5,10 +5,10 @@ namespace Drupal\Core;
 use Drupal\Core\Render\BubbleableMetadata;
 
 /**
- * Used for returning generated URLs and their associated bubbleable metadata.
+ * Used to return generated URLs, along with associated bubbleable metadata.
  *
- * Note: Do not confuse this with \Drupal\Core\Url. That's typically used for
- *  managing URLs that are not yet generated (generally includes route name and route parameters).
+ * Note: not to be confused with \Drupal\Core\Url, which is for passing around
+ *    URLs that are not generated (typically route name + route parameters).
  */
 class GeneratedUrl extends BubbleableMetadata {
 
