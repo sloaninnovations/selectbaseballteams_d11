a9449b9eebe8f56c48ed443364a736dcb09bae8d
Wim Leers <wim.leers@acquia.com>
2024-02-20 17:11:10 +0100

    Update `::isMutableResourceType()` to support fully validatable config entity types => will generate necessary routes 🚀

--- core/modules/jsonapi/src/ResourceType/ResourceTypeRepository.php
+++ core/modules/jsonapi/src/ResourceType/ResourceTypeRepository.php
@@ -355,7 +355,16 @@ class ResourceTypeRepository implements ResourceTypeRepositoryInterface {
    */
   protected static function isMutableResourceType(EntityTypeInterface $entity_type, $bundle) {
     assert(is_string($bundle) && !empty($bundle), 'A bundle ID is required. Bundleless entity types should pass the entity type ID again.');
-    return !$entity_type instanceof ConfigEntityTypeInterface;
+    if (!$entity_type instanceof ConfigEntityTypeInterface) {
+      return TRUE;
+    }
+
+    // @todo this is inaccurate — it won't work for e.g. `field.field.*.*.*`.
+    $config_schema_type_name = $entity_type->getConfigPrefix() . '.*';
+    $config_schema_type_definition = \Drupal::service('config.typed')->getDefinition($config_schema_type_name);
+
+    // Config entities are only mutable if they're fully validatable.
+    return array_key_exists('FullyValidatable', $config_schema_type_definition['constraints'] ?? []);
   }
 
   /**
