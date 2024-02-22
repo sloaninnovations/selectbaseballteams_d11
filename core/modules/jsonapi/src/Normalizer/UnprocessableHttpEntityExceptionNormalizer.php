ea97b7c0c2adc58dfb1bf919fb34aa723a06ba8f
Wim Leers <wim.leers@acquia.com>
2024-02-22 14:56:19 +0100

    AFAICT 422 responses for entity-level validation errors should have the same links as any others? 🤔

--- core/modules/jsonapi/src/Normalizer/UnprocessableHttpEntityExceptionNormalizer.php
+++ core/modules/jsonapi/src/Normalizer/UnprocessableHttpEntityExceptionNormalizer.php
@@ -34,7 +34,6 @@ class UnprocessableHttpEntityExceptionNormalizer extends HttpExceptionNormalizer
     /** @var \Drupal\jsonapi\Exception\UnprocessableHttpEntityException $exception */
     $errors = parent::buildErrorObjects($exception);
     $error = $errors[0];
-    unset($error['links']);
 
     $errors = [];
     $violations = $exception->getViolations();
