1551071a35989fab9ccdeccfb20a85199c5625c1
Wim Leers <wim.leers@acquia.com>
2024-02-23 13:55:04 +0100

    Clean-up.

--- core/modules/jsonapi/tests/src/Functional/ConfigEntityResourceTestBase.php
+++ core/modules/jsonapi/tests/src/Functional/ConfigEntityResourceTestBase.php
@@ -2,13 +2,10 @@
 
 namespace Drupal\Tests\jsonapi\Functional;
 
-use Drupal\Core\Config\TypedConfigManagerInterface;
-use Drupal\Core\Validation\Plugin\Validation\Constraint\FullyValidatableConstraint;
-
 /**
  * Resource test base class for config entities.
  *
- * @todo Remove this in https://www.drupal.org/node/2300677.
+ * @todo Simplify this in https://www.drupal.org/node/3423459 or https://www.drupal.org/node/3423462.
  */
 abstract class ConfigEntityResourceTestBase extends ResourceTestBase {
 
@@ -18,8 +15,11 @@ abstract class ConfigEntityResourceTestBase extends ResourceTestBase {
    * @var array
    */
   const SKIP_METHODS = [
+    // @todo Remove in https://www.drupal.org/node/3423462: the FieldConfigs using this FieldStorageConfig
     'testRelated',
+    // @todo Remove in https://www.drupal.org/node/3423462: the `instances` (FieldConfigs) of this FieldStorageConfig
     'testRelationships',
+    // @todo Remove in https://www.drupal.org/node/3423459.
     'testDeleteIndividual',
     'testRevisions',
   ];
@@ -33,32 +33,6 @@ abstract class ConfigEntityResourceTestBase extends ResourceTestBase {
       $this->markTestSkipped("Not yet supported for config entities.");
     }
     parent::setUp();
-  }
-
-  /**
-   * Whether the tested config entity type is fully validatable.
-   *
-   * @return bool
-   *   Whether the tested config entity type is fully validatable.
-   *
-   * @see \Drupal\KernelTests\Core\Config\ConfigEntityValidationTestBase::isFullyValidatable()
-   */
-  protected function isFullyValidatable(): bool {
-    $typed_config = $this->container->get('config.typed');
-    assert($typed_config instanceof TypedConfigManagerInterface);
-    // @see \Drupal\Core\Entity\Plugin\DataType\ConfigEntityAdapter::getConfigTypedData()
-    $config_entity_type_schema_constraints = $typed_config
-      ->createFromNameAndData(
-        $this->entity->getConfigDependencyName(),
-        $this->entity->toArray()
-      )->getConstraints();
-
-    foreach ($config_entity_type_schema_constraints as $constraint) {
-      if ($constraint instanceof FullyValidatableConstraint) {
-        return TRUE;
-      }
-    }
-    return FALSE;
   }
 
 }
--- core/modules/jsonapi/tests/src/Functional/ResourceTestBase.php
+++ core/modules/jsonapi/tests/src/Functional/ResourceTestBase.php
@@ -12,6 +12,7 @@ use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Cache\CacheableResponseInterface;
 use Drupal\Core\Cache\CacheRedirect;
 use Drupal\Core\Config\Entity\ConfigEntityInterface;
+use Drupal\Core\Config\TypedConfigManagerInterface;
 use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\Core\Entity\ContentEntityNullStorage;
 use Drupal\Core\Entity\ContentEntityTypeInterface;
@@ -33,6 +34,7 @@ use Drupal\Core\TypedData\Plugin\DataType\StringData;
 use Drupal\Core\TypedData\Plugin\DataType\Timestamp;
 use Drupal\Core\TypedData\TypedDataInternalPropertiesHelper;
 use Drupal\Core\Url;
+use Drupal\Core\Validation\Plugin\Validation\Constraint\FullyValidatableConstraint;
 use Drupal\field\Entity\FieldConfig;
 use Drupal\field\Entity\FieldStorageConfig;
 use Drupal\jsonapi\CacheableResourceResponse;
@@ -276,7 +278,7 @@ abstract class ResourceTestBase extends BrowserTestBase {
       }
       static::$patchProtectedFieldNames = array_flip($this->entity->getEntityType()->getConstraints()['ImmutableProperties']);
       // Abort ASAP to prevent unnecessary use of resources.
-      if ($this instanceof ConfigEntityResourceTestBase && !$this->isFullyValidatable()) {
+      if ($this instanceof ConfigEntityResourceTestBase && !$this->isFullyValidatableConfigEntityType()) {
         $this->markTestSkipped("Not yet supported for config entities.");
         return;
       }
@@ -3706,6 +3708,32 @@ abstract class ResourceTestBase extends BrowserTestBase {
       unset($expected_document['data']['attributes'][$field_name]);
     }
     return $expected_document;
+  }
+
+  /**
+   * Whether the tested config entity type is fully validatable.
+   *
+   * @return bool
+   *   Whether the tested config entity type is fully validatable.
+   *
+   * @see \Drupal\KernelTests\Core\Config\ConfigEntityValidationTestBase::isFullyValidatable()
+   */
+  protected function isFullyValidatableConfigEntityType(): bool {
+    $typed_config = $this->container->get('config.typed');
+    assert($typed_config instanceof TypedConfigManagerInterface);
+    // @see \Drupal\Core\Entity\Plugin\DataType\ConfigEntityAdapter::getConfigTypedData()
+    $config_entity_type_schema_constraints = $typed_config
+      ->createFromNameAndData(
+        $this->entity->getConfigDependencyName(),
+        $this->entity->toArray()
+      )->getConstraints();
+
+    foreach ($config_entity_type_schema_constraints as $constraint) {
+      if ($constraint instanceof FullyValidatableConstraint) {
+        return TRUE;
+      }
+    }
+    return FALSE;
   }
 
 }
