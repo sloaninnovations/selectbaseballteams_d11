851637c981bf1f19efb505d695c604a0d41e5b42
IMMACULATE X <58532-immaculate.x@users.noreply.drupalcode.org>
2024-03-28 07:35:15 +0000

    Update 4 files
    - /core/modules/jsonapi/src/Controller/EntityResource.php
    - /core/modules/jsonapi/tests/modules/jsonapi_test_entity_query_alter/jsonapi_test_entity_query_alter.info.yml
    - /core/modules/jsonapi/tests/modules/jsonapi_test_entity_query_alter/jsonapi_test_entity_query_alter.module
    - /core/modules/jsonapi/tests/src/Functional/JsonApiFunctionalEntityQueryAlterTest.php

---
+++ core/modules/jsonapi/tests/src/Functional/JsonApiFunctionalEntityQueryAlterTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Drupal\Tests\jsonapi\Functional;
+
+use Drupal\Component\Serialization\Json;
+use Drupal\Core\Url;
+use Drupal\jsonapi\Query\OffsetPage;
+use Drupal\node\Entity\Node;
+
+/**
+ * General functional test class.
+ *
+ * @group jsonapi
+ * @group legacy
+ *
+ * @internal
+ */
+class JsonApiFunctionalEntityQueryAlterTest extends JsonApiFunctionalTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = [
+    'jsonapi_test_entity_query_alter',
+    'page_cache',
+  ];
+
+  /**
+   * Test the GET method.
+   */
+  public function testRead() {
+    $this->createDefaultContent(2, 2, TRUE, TRUE, static::IS_NOT_MULTILINGUAL, FALSE);
+    // 0. HEAD request allows a client to verify that JSON:API is installed and no major error is introduced
+    // by the query alterer.
+    $this->httpClient->request('HEAD', $this->buildUrl('/jsonapi/node/article'));
+    $this->assertSession()->statusCodeEquals(200);
+
+    $collection_output = Json::decode($this->drupalGet('/jsonapi/node/article'));
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertEquals(2, count($collection_output['data']));
+
+    $collection_output = Json::decode($this->drupalGet('/jsonapi/node/article', [
+      'query' =>  [
+        'sort' => 'drupal_internal__nid',
+        'custom_nid' => 1
+      ],
+    ]));
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertEquals(1, count($collection_output['data']));
+    $this->assertEquals(1, $collection_output['data'][0]['attributes']['drupal_internal__nid']);
+
+    $collection_output = Json::decode($this->drupalGet('/jsonapi/node/article', [
+      'query' =>  [
+        'sort' => 'drupal_internal__nid',
+        'custom_nid' => 2
+      ],
+    ]));
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertEquals(1, count($collection_output['data']));
+    $this->assertEquals(2, $collection_output['data'][0]['attributes']['drupal_internal__nid']);
+
+  }
+}
