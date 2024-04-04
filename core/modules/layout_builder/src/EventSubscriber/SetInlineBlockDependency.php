ca3d6c73895bede9fcffbf8faaf85a46a66b015a
Tadej Baša <tadej.basa@gmail.com>
2024-04-04 11:25:50 +0200

    Issue #3047022: revert constructor property promotion.

--- core/modules/layout_builder/src/EventSubscriber/SetInlineBlockDependency.php
+++ core/modules/layout_builder/src/EventSubscriber/SetInlineBlockDependency.php
@@ -66,7 +66,7 @@ class SetInlineBlockDependency implements EventSubscriberInterface {
     mixed $entityRepository,
     protected readonly Connection $database,
     protected readonly InlineBlockUsageInterface $usage,
-    protected readonly SectionStorageManagerInterface $sectionStorageManager,
+    SectionStorageManagerInterface $sectionStorageManager,
     protected readonly ?RouteMatchInterface $currentRouteMatch,
   ) {
     if (!$entityRepository instanceof EntityRepositoryInterface) {
@@ -75,6 +75,7 @@ class SetInlineBlockDependency implements EventSubscriberInterface {
       $entityRepository = \Drupal::service('entity.repository');
     }
     $this->entityRepository = $entityRepository;
+    $this->sectionStorageManager = $sectionStorageManager;
     if (empty($currentRouteMatch)) {
       // @todo Replace link with a link to the change record.
       @trigger_error('Calling ' . __METHOD__ . ' without the $currentRouteMatch argument is deprecated in drupal:11.0.0 and will be required in drupal:12.0.0. See https://www.drupal.org/node/3047022', E_USER_DEPRECATED);
