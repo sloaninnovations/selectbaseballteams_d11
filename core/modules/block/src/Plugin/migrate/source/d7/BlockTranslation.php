6f45e3b2f841e6c56ece2f47edf8cd78ea431940
quietone <quietone@2572884.no-reply.drupal.org>
2024-04-04 16:13:39 +1300

    format d7/BlockTranslation.php

--- core/modules/block/src/Plugin/migrate/source/d7/BlockTranslation.php
+++ core/modules/block/src/Plugin/migrate/source/d7/BlockTranslation.php
@@ -3,6 +3,7 @@
 namespace Drupal\block\Plugin\migrate\source\d7;
 
 use Drupal\block\Plugin\migrate\source\Block;
+use Drupal\migrate\Attribute\MigrateSource;
 
 // cspell:ignore objectid objectindex plid textgroup
 
@@ -14,7 +15,10 @@ use Drupal\block\Plugin\migrate\source\Block;
  * @see \Drupal\migrate\Plugin\migrate\source\SqlBase
  * @see \Drupal\migrate\Plugin\migrate\source\SourcePluginBase
  */
-#[\Drupal\migrate\Attribute\MigrateSource(id: 'd7_block_translation', source_module: 'i18n_block')]
+#[MigrateSource(
+  id: 'd7_block_translation',
+  source_module: 'i18n_block',
+)]
 class BlockTranslation extends Block {
 
   /**
