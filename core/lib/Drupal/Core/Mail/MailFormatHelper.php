cc37ae7c8f2b8a6583acd991bff541e258f7f800
Pamela Barone <pam@technocrat.com.au>
2024-05-03 14:30:41 +1000

    Adds a comment to the regex

--- core/lib/Drupal/Core/Mail/MailFormatHelper.php
+++ core/lib/Drupal/Core/Mail/MailFormatHelper.php
@@ -339,6 +339,7 @@ class MailFormatHelper {
         [, , $url, $label] = $match;
         // Ensure all URLs are absolute.
         static::$urls[] = strpos($url, '://') ? $url : preg_replace(static::$regexp, $base_url . '/', $url);
+        // Strip newlines from the label.
         return preg_replace('/\r?\n|\r/', '', $label) . ' [' . count(static::$urls) . ']';
       }
     }
