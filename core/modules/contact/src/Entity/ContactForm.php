8d67560f173873e3e2fe3ffbbe693843bbb6e7de
Andrey Postnikov <2684-andypost@users.noreply.drupalcode.org>
2024-05-20 06:12:52 +0000

    Applied Andypost's suggestions

--- core/modules/contact/src/Entity/ContactForm.php
+++ core/modules/contact/src/Entity/ContactForm.php
@@ -78,7 +78,7 @@ class ContactForm extends ConfigEntityBundleBase implements ContactFormInterface
    *
    * @var string|null
    */
-  protected $message = NULL;
+  protected ?string $message = NULL;
 
   /**
    * List of recipient email addresses.
@@ -92,14 +92,14 @@ class ContactForm extends ConfigEntityBundleBase implements ContactFormInterface
    *
    * @var string
    */
-  protected $redirect;
+  protected ?string $redirect = NULL;
 
   /**
    * An auto-reply message.
    *
    * @var string|null
    */
-  protected $reply = NULL;
+  protected ?string $reply = NULL;
 
   /**
    * The weight of the category.
