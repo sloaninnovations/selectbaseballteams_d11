4b87d6900aa56cc2adf0f29c897ed7d3d506f0bb
narendra-drupal <87118318+narendra-drupal@users.noreply.github.com>
2024-05-21 18:23:57 +0530

    Fixed EntityFormModeValidationTest

--- core/tests/Drupal/KernelTests/Core/Entity/EntityFormModeValidationTest.php
+++ core/tests/Drupal/KernelTests/Core/Entity/EntityFormModeValidationTest.php
@@ -18,7 +18,12 @@ class EntityFormModeValidationTest extends ConfigEntityValidationTestBase {
   /**
    * {@inheritdoc}
    */
-  protected static $modules = ['user'];
+  protected static $modules = ['user', 'entity_test'];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static array $propertiesWithOptionalValues = ['description'];
 
   /**
    * {@inheritdoc}
@@ -41,7 +46,51 @@ class EntityFormModeValidationTest extends ConfigEntityValidationTestBase {
    */
   public function testImmutableProperties(array $valid_values = []): void {
     $valid_values['id'] = 'user.test_changed';
+    $valid_values['targetEntityType'] = 'entity_test';
     parent::testImmutableProperties($valid_values);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function providerInvalidMachineNameCharacters(): array {
+    return [
+      'INVALID: contains a space' => ['prefix.space separated', FALSE],
+      'INVALID: dash separated' => ['prefix.dash-separated', FALSE],
+      'INVALID: uppercase letters' => ['Uppercase.Letters', FALSE],
+      'VALID: underscore separated' => ['prefix.underscore_separated', TRUE],
+      'VALID: contains numbers' => ['prefix1.part2', TRUE],
+    ];
+  }
+
+  /**
+   * Tests that the entity ID's length is validated if it is a machine name.
+   *
+   * @param string $prefix
+   *   Prefix for machine name.
+   */
+  public function testMachineNameLength(string $prefix = ''): void {
+    // Machine name(id) of entity view mode is of type
+    // 'entity_type.view_mode_type'.
+    $string = 'test.';
+    parent::testMachineNameLength($string);
+  }
+
+  /**
+   * Tests that description can be NULL, but not empty strings.
+   */
+  public function testDescriptionCannotBeEmpty(): void {
+    $this->entity->set('description', NULL);
+    // The entity's getters should cast NULL values to empty strings.
+    $this->assertSame('', $this->entity->getDescription());
+    // But NULL values should be valid at the config level.
+    $this->assertValidationErrors([]);
+
+    // But they cannot be empty strings, because that doesn't make sense.
+    $this->entity->set('description', '');
+    $this->assertValidationErrors([
+      'description' => 'This value should not be blank.',
+    ]);
   }
 
 }
