ce6dbaddb197bc3004b191553e8cc58fd9bce8aa
narendra-drupal <87118318+narendra-drupal@users.noreply.github.com>
2024-05-27 16:04:54 +0530

    Fixed failing tests

--- core/tests/Drupal/FunctionalTests/Rest/EntityFormModeResourceTestBase.php
+++ core/tests/Drupal/FunctionalTests/Rest/EntityFormModeResourceTestBase.php
@@ -40,7 +40,7 @@ abstract class EntityFormModeResourceTestBase extends ConfigEntityResourceTestBa
     $entity_form_mode = EntityFormMode::create([
       'id' => 'user.test',
       'label' => 'Test',
-      'description' => '',
+      'description' => NULL,
       'targetEntityType' => 'user',
     ]);
     $entity_form_mode->save();
