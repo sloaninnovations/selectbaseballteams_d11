59bde3dcef4fea4bd3e96d279aa38b61f3723c5a
yashrode774 <yashrode774@gmail.com>
2024-06-14 16:25:50 +0530

    deprecation for old key

--- core/modules/toolbar/js/escapeAdmin.js
+++ core/modules/toolbar/js/escapeAdmin.js
@@ -5,7 +5,17 @@
 
 (function ($, Drupal, drupalSettings) {
   const pathInfo = drupalSettings.path;
-  const escapeAdminPath = sessionStorage.getItem('escapeAdminPath');
+  const oldEscapeAdminPath = sessionStorage.getItem('escapeAdminPath');
+  if (oldEscapeAdminPath) {
+    Drupal.deprecationError({
+      message:
+        'escapeAdminPath is deprecated in drupal:11.x and will be removed from drupal:12.0.0.',
+    });
+  }
+  const escapeAdminPath =
+    oldEscapeAdminPath ??
+    sessionStorage.getItem('Drupal.toolbar.escapeAdminPath');
+
   const windowLocation = window.location;
 
   // Saves the last non-administrative page in the browser to be able to link
@@ -16,7 +26,7 @@
     !pathInfo.currentPathIsAdmin &&
     !/destination=/.test(windowLocation.search)
   ) {
-    sessionStorage.setItem('escapeAdminPath', windowLocation);
+    sessionStorage.setItem('Drupal.toolbar.escapeAdminPath', windowLocation);
   }
 
   /**
