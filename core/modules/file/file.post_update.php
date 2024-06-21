60f3dde45c1700c79e801f52afc5fb0e11a807be
sleitner <38752-sleitner@users.noreply.drupalcode.org>
2024-06-21 20:59:09 +0000

    Issue #2954834 by Anybody, Grevil, sleitner, BS Pavan, pbouchereau, joel_osc, dhirendra.mishra, Mistrae: Add poster image to HTML5 media videos

--- core/modules/file/file.post_update.php
+++ core/modules/file/file.post_update.php
@@ -1,8 +1,5 @@
 <?php
 
-use Drupal\Core\Config\Entity\ConfigEntityUpdater;
-use Drupal\user\RoleInterface;
-
 /**
  * @file
  * Post update functions for File.
@@ -17,33 +14,6 @@ function file_removed_post_updates() {
     'file_post_update_add_permissions_to_roles' => '11.0.0',
     'file_post_update_add_default_filename_sanitization_configuration' => '11.0.0',
   ];
-}
-
-/**
- * Grant all non-anonymous roles the 'delete own files' permission.
- */
-function file_post_update_add_permissions_to_roles(?array &$sandbox = NULL): void {
-  \Drupal::classResolver(ConfigEntityUpdater::class)->update($sandbox, 'user_role', function (RoleInterface $role): bool {
-    if ($role->id() === RoleInterface::ANONYMOUS_ID || $role->isAdmin()) {
-      return FALSE;
-    }
-    $role->grantPermission('delete own files');
-    return TRUE;
-  });
-}
-
-/**
- * Add default filename sanitization configuration.
- */
-function file_post_update_add_default_filename_sanitization_configuration() {
-  $config = \Drupal::configFactory()->getEditable('file.settings');
-  $config->set('filename_sanitization.transliterate', FALSE);
-  $config->set('filename_sanitization.replace_whitespace', FALSE);
-  $config->set('filename_sanitization.replace_non_alphanumeric', FALSE);
-  $config->set('filename_sanitization.deduplicate_separators', FALSE);
-  $config->set('filename_sanitization.lowercase', FALSE);
-  $config->set('filename_sanitization.replacement_character', '-');
-  $config->save();
 }
 
 function file_post_update_add_poster_image() {
