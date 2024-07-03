10a4b0a7ed978b839c6e7bb4c10ed8df406fa5b0
narendra-drupal <87118318+narendra-drupal@users.noreply.github.com>
2024-07-03 15:57:04 +0530

    Updated comment

--- core/modules/contact/src/ContactFormEditForm.php
+++ core/modules/contact/src/ContactFormEditForm.php
@@ -137,8 +137,7 @@ class ContactFormEditForm extends EntityForm implements ContainerInjectionInterf
    */
   protected function copyFormValuesToEntity(EntityInterface $entity, array $form, FormStateInterface $form_state) {
     // Config schema dictates that the reply, redirect, and message values
-    // cannot be empty strings. So, if they're empty, remove them from the
-    // submitted values so they aren't copied to the entity.
+    // cannot be empty strings. So, if they're empty, make them NULL.
     if ($form_state->hasValue('reply') && trim($form_state->getValue('reply')) === '') {
       $form_state->setValue('reply', NULL);
     }
