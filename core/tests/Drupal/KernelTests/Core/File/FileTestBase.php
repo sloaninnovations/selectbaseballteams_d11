c5977d2f6261d53ca28e33937071b05cef808188
yashrode774 <yashrode774@gmail.com>
2024-07-04 15:37:30 +0530

    kernel test fix

--- core/tests/Drupal/KernelTests/Core/File/FileTestBase.php
+++ core/tests/Drupal/KernelTests/Core/File/FileTestBase.php
@@ -43,7 +43,11 @@ abstract class FileTestBase extends KernelTestBase {
     // file_default_scheme(). As we are creating the configuration here remove
     // the global override.
     unset($GLOBALS['config']['system.file']);
-    \Drupal::configFactory()->getEditable('system.file')->set('default_scheme', 'public')->save();
+    \Drupal::configFactory()->getEditable('system.file')
+      ->set('default_scheme', 'public')
+      ->set('allow_insecure_uploads', TRUE)
+      ->set('temporary_maximum_age', 21600)
+      ->save();
   }
 
   /**
