b157cec7516ec02f579349b093365eb67ab1d517
Richard Allen <alexanderallen@351784.no-reply.drupal.org>
2024-07-11 09:22:08 -0400

    Issue #3425201 by Kingdutch, Alexander Allen: Separate assertions into individual unit tests.

--- core/tests/Drupal/Tests/Core/ResultTest.php
+++ core/tests/Drupal/Tests/Core/ResultTest.php
@@ -15,22 +15,44 @@ use PHPUnit\Framework\Attributes\CoversClass;
 class ResultTest extends UnitTestCase {
 
   /**
-   * Tests the `\Drupal\Core\Result` type.
+   * Tests that a result of type Ok is of type Ok.
    */
-  public function testResultType(): void {
-
+  public function testThatOkResultIsOk(): void {
     $this->assertTrue(Result::ok("Foo")->isOk());
+  }
 
-    $this->assertTrue(Result::ok("Foo")->isOk());
-
+  /**
+   * Tests that a result of type Ok is not of type Error.
+   */
+  public function testThatOkResultIsNotError(): void {
     $this->assertFalse(Result::ok("Foo")->isError());
+  }
 
+  /**
+   * Tests that a result of type Ok containing an int returns an int.
+   */
+  public function testThatOkResultOfTypeIntReturnsInt(): void {
     $this->assertEquals(Result::ok(5)->getValue(), 5);
+  }
 
+  /**
+   * Tests that a result of type Error is not of type Ok.
+   */
+  public function testThatErrorResultIsNotOk(): void {
     $this->assertFalse(Result::error("Foo")->isOk());
+  }
 
+  /**
+   * Test that a result of type Error is of type Error.
+   */
+  public function testThatErrorResultIsError(): void {
     $this->assertTrue(Result::error("Foo")->isError());
+  }
 
+  /**
+   * Tests that a result containing a complext object returns the object.
+   */
+  public function testThatErrorResultWithComplexObjectReturnsComplexObject(): void {
     $complexObject = new \stdClass();
     $this->assertEquals(Result::error($complexObject)->getValue(), $complexObject);
   }
