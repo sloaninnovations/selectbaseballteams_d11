86940d7bb3d1d9ec71103736431013eb2ea6d76c
iberdinsky <iberdinsky@skilld.cloud>
2024-07-14 15:57:19 +0200

    Backported changes from Navigation module

--- core/misc/drupal-filter.js
+++ core/misc/drupal-filter.js
@@ -4,6 +4,38 @@
  */
 
 (function (Drupal, debounce, once) {
+  const FILTER_EVENT = 'drupal-filter-event';
+
+  function hideElement(element) {
+    element.classList.add('hidden');
+    element.setAttribute('hidden', true);
+    element.style.display = 'none';
+  }
+
+  function showElement(element) {
+    element.classList.remove('hidden');
+    element.removeAttribute('hidden');
+    element.style.display = 'revert';
+  }
+
+  function searchMethod(target, query) {
+    return target?.textContent.toLowerCase().includes(query);
+  }
+
+  function foundInItem(query, item) {
+    let found = false;
+    if (item.searchTargets) {
+      item.searchTargets.forEach((target) => {
+        if (searchMethod(target, query)) {
+          found = true;
+        }
+      });
+    } else {
+      found = searchMethod(item, query);
+    }
+    return found;
+  };
+
   /**
    * Filters the table by a text input search string.
    *
@@ -32,38 +64,12 @@
    */
   Drupal.behaviors.drupalFilterByText = {
     attach(context, settings) {
-      const hideElement = (element) => {
-        element.classList.add('hidden');
-        element.setAttribute('hidden', true);
-        element.style.display = 'none';
-      };
-
-      const showElement = (element) => {
-        element.classList.remove('hidden');
-        element.removeAttribute('hidden');
-        element.style.display = 'revert';
-      };
-
-      const searchMethod = (target, query) =>
-        target.textContent.toLowerCase().includes(query);
-
-      const foundInItem = (query, item) => {
-        let found = false;
-        if (item.searchTargets) {
-          item.searchTargets.forEach((target) => {
-            if (searchMethod(target, query)) {
-              found = true;
-            }
-          });
-        } else {
-          found = searchMethod(item, query);
-        }
-        return found;
-      };
 
       once('drupal-filter-text', '.table-filter-text', context).forEach(
         (input) => {
           const { table, items, targets, singular, plural } = input.dataset;
+
+          const ALL_PHRASE = `All available ${plural || 'items'} are listed.`;
 
           const SINGULAR_PHRASE = `1 ${
             singular || 'item'
@@ -83,97 +89,107 @@
           };
 
           // Table can be in another context so we have to search in document.
-          // Can be 2 tables on views page.
-          once('drupal-filter-text', table, document).forEach(
-            (tableElement) => {
-              if (items) {
-                const filterItems = tableElement.querySelectorAll(items);
+          const tables = document.querySelectorAll(table);
 
-                // If we need to search deeper in row elements.
-                if (targets) {
-                  // To avoid search in item on every query let's store them in item object.
-                  filterItems.forEach((item) => {
-                    item.searchTargets = item.querySelectorAll(targets);
-                  });
-                }
+          const initTable = (tableElement) => {
+            const filterItems = tableElement.querySelectorAll(items);
 
-                const labels = tableElement.querySelectorAll(
-                  '[data-filter-label]',
-                );
-                labels.forEach((label) => {
-                  label.labelledItems = tableElement.querySelectorAll(
-                    `[data-filter-labelledby="${label.dataset.filterLabel}"]`,
-                  );
-                });
+            // If we need to search deeper in row elements.
+            if (targets) {
+              // To avoid search in item on every query let's store them in item object.
+              filterItems.forEach((item) => {
+                item.searchTargets = item.querySelectorAll(targets);
+              });
+            }
+            const labels = tableElement.querySelectorAll('[data-filter-label]');
 
-                const checkLabels = (reset = false) => {
-                  labels.forEach((label) => {
-                    const hasVisible = Array.from(label.labelledItems).some(
-                      (element) => {
-                        return !element.hasAttribute('hidden');
-                      },
-                    );
-                    if (hasVisible) {
-                      if (label.nodeName === 'DETAILS') {
-                        // Found in closed details.
-                        if (!reset && !label.hasAttribute('open')) {
-                          label.setAttribute('open', true);
-                          label.setAttribute('opened-by-filter', true);
-                        } else if (
-                          reset &&
-                          label.hasAttribute('opened-by-filter')
-                        ) {
-                          label.removeAttribute('open');
-                          label.removeAttribute('opened-by-filter');
-                        }
-                      }
-                      showElement(label);
-                    } else {
-                      hideElement(label);
+            labels.forEach((label) => {
+              label.labelledItems = tableElement.querySelectorAll(
+                `[data-filter-labelledby="${label.dataset.filterLabel}"]`,
+              );
+            });
+
+            const checkLabels = (reset = false) => {
+              labels.forEach((label) => {
+                if (
+                  Array.from(label.labelledItems).some(
+                    (element) => !element.hasAttribute('hidden'),
+                  )
+                ) {
+                  if (label.nodeName === 'DETAILS') {
+                    if (!reset && !label.hasAttribute('open')) {
+                      label.setAttribute('open', true);
+                      label.setAttribute('opened-by-filter', true);
+                    } else if (
+                      reset &&
+                      label.hasAttribute('opened-by-filter')
+                    ) {
+                      label.removeAttribute('open');
+                      label.removeAttribute('opened-by-filter');
                     }
-                  });
-                };
-
-                const filterTableList = (e) => {
-                  const query = e.target.value.toLowerCase();
-
-                  // Filter if the length of the query is at least 2 characters.
-                  if (query.length >= 2) {
-                    let matches = 0;
-
-                    filterItems.forEach((item) => {
-                      if (!foundInItem(query, item)) {
-                        hideElement(item);
-                      } else {
-                        showElement(item);
-                        matches++;
-                      }
-                    });
-
-                    makeAnnounce(matches);
-                    checkLabels();
-                  } else {
-                    Drupal.announce(
-                      `All available ${plural || 'items'} are listed.`,
-                    );
-                    filterItems.forEach((item) => {
-                      showElement(item);
-                    });
-                    checkLabels(true);
                   }
-                };
+                  showElement(label);
+                } else {
+                  hideElement(label);
+                }
+              });
+            };
 
-                input.addEventListener('input', debounce(filterTableList, 200));
+            const filterTableList = (e) => {
+              const query = e.target.value.toLowerCase();
 
-                input.addEventListener('keydown', (e) => {
-                  if (e.which === 13) {
-                    e.preventDefault();
-                    e.stopPropagation();
+              // Filter if the length of the query is at least 2 characters.
+              if (query.length >= 2) {
+                let matches = 0;
+
+                filterItems.forEach((item) => {
+                  if (!foundInItem(query, item)) {
+                    hideElement(item);
+                  } else {
+                    showElement(item);
+                    matches += 1;
                   }
                 });
+
+                makeAnnounce(matches);
+                checkLabels();
+              } else {
+                Drupal.announce(ALL_PHRASE);
+                filterItems.forEach((item) => {
+                  showElement(item);
+                });
+                checkLabels(true);
               }
-            },
+            };
+
+            tableElement.addEventListener(FILTER_EVENT, (e) =>
+              filterTableList(e.detail.event),
+            );
+          }
+
+          tables.forEach((tableElement) => initTable(tableElement));
+
+          input.addEventListener(
+            'input',
+            debounce((event) => {
+              tables.forEach((tableElement) => {
+                tableElement.dispatchEvent(
+                  new CustomEvent(FILTER_EVENT, {
+                    detail: {
+                      event,
+                    },
+                  }),
+                );
+              });
+            }, 200),
           );
+
+          input.addEventListener('keydown', (e) => {
+            if (e.which === 13) {
+              e.preventDefault();
+              e.stopPropagation();
+            }
+          });
         },
       );
     },
