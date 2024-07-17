14c9f689fed9af95840a38cff1c1f7778cb83047
Joe Parsons <parsonsj@arizona.edu>
2024-07-16 20:41:59 -0700

    Fix typo.

--- core/modules/content_moderation/tests/src/Kernel/EntityTypeInfoTest.php
+++ core/modules/content_moderation/tests/src/Kernel/EntityTypeInfoTest.php
@@ -66,7 +66,7 @@ class EntityTypeInfoTest extends KernelTestBase {
     $this->installEntitySchema('action');
     $this->installEntitySchema('entity_test');
     $this->installEntitySchema('entity_test_with_bundle');
-    $this->installEnitiySchema('user');
+    $this->installEntitySchema('user');
 
     $this->installConfig(['content_moderation']);
   }
