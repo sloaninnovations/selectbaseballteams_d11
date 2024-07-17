9b0d2e20d08c707ef24fdeb6f50eec2a543da047
Joe Parsons <parsonsj@arizona.edu>
2024-07-16 20:37:43 -0700

    More test fixes.

--- core/modules/content_moderation/tests/src/Kernel/EntityTypeInfoTest.php
+++ core/modules/content_moderation/tests/src/Kernel/EntityTypeInfoTest.php
@@ -29,6 +29,7 @@ class EntityTypeInfoTest extends KernelTestBase {
     'workflows',
     'entity_test',
     'system',
+    'user',
   ];
 
   /**
@@ -65,6 +66,7 @@ class EntityTypeInfoTest extends KernelTestBase {
     $this->installEntitySchema('action');
     $this->installEntitySchema('entity_test');
     $this->installEntitySchema('entity_test_with_bundle');
+    $this->installEnitiySchema('user');
 
     $this->installConfig(['content_moderation']);
   }
