b3643cdc59cac1c823fde9876ce67d003e6333ca
Joël Pittet <pittet@cs.ubc.ca>
2024-07-23 15:04:27 -0700

    Remove duplicate trim() call, now inline on ternary

--- core/modules/update/src/UpdateSettingsForm.php
+++ core/modules/update/src/UpdateSettingsForm.php
@@ -126,7 +126,6 @@ class UpdateSettingsForm extends ConfigFormBase {
    *   The value to be stored in config.
    */
   public static function multiLineStringToArray(string $value): array {
-    $value = trim($value);
     return trim($value) ? array_map('trim', explode("\n", $value)) : [];
   }
 
