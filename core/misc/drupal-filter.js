7c6b0295700965de9669fa147d2015600d6d5480
Marcos Buarque de Hollanda <marcos.buarque@uol.com.br>
2024-09-14 23:44:48 -0300

    Activate the filter only when input is focused.

--- core/misc/drupal-filter.js
+++ core/misc/drupal-filter.js
@@ -65,166 +65,173 @@
             searchStart = false,
           } = input.dataset;
 
-          const ALL_PHRASE = Drupal.t('All available items are listed.');
-          const SINGULAR_PHRASE = '1 item is available in the modified list.';
-          const PLURAL_PHRASE =
-            '@count items are available in the modified list.';
+          function activateFilter(e) {
+            const ALL_PHRASE = Drupal.t('All available items are listed.');
+            const SINGULAR_PHRASE = '1 item is available in the modified list.';
+            const PLURAL_PHRASE =
+              '@count items are available in the modified list.';
 
-          const makeAnnounce = (matches) => {
-            Drupal.announce(
-              Drupal.formatPlural(
-                matches,
-                singular || SINGULAR_PHRASE,
-                plural || PLURAL_PHRASE,
-              ),
-            );
-          };
-
-          function normalizeString(str) {
-            return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
-          }
-
-          const reStartsWith = (query) => new RegExp(`\\b${query}`);
-
-          function searchMethod(target, query) {
-            return (searchStart === 'true')
-              ? target.search(reStartsWith(query)) !== -1
-              : target.includes(query);
-          }
-
-          // Table can be in another context so we have to search in document.
-          const tables = document.querySelectorAll(table);
-
-          const initTable = (tableElement) => {
-
-            // Transitional information helping accelerate queries.
-            let filterItemsTransitional = null;
-            let queryTransitional = "";
-
-            // Prepare items for accelerated filtering.
-            const filterItems = Array.from(
-              tableElement.querySelectorAll(items)
-            ).map((source) => {
-
-              // If we need to search deeper in row elements.
-              const textContent = (targets)
-                ?
-                  Array.from(
-                    source.querySelectorAll(targets)
-                  ).reduce((acc, target) => {
-                    return acc + ' ' + target.textContent;
-                  }, ' ').trim()
-                :
-                  source.textContent;
-
-              return {
-                node: source,
-                textContent: normalizeString(textContent),
-              };
-            });
-
-            filterItemsTransitional = filterItems;
-
-            const labels = tableElement.querySelectorAll('[data-filter-label]');
-
-            labels.forEach((label) => {
-              label.labelledItems = tableElement.querySelectorAll(
-                `[data-filter-labelledby="${label.dataset.filterLabel}"]`,
+            const makeAnnounce = (matches) => {
+              Drupal.announce(
+                Drupal.formatPlural(
+                  matches,
+                  singular || SINGULAR_PHRASE,
+                  plural || PLURAL_PHRASE,
+                ),
               );
-            });
+            };
 
-            const checkLabels = (reset = false) => {
-              labels.forEach((label) => {
-                if (
-                  Array.from(label.labelledItems).some(
-                    (element) => !element.hasAttribute('hidden'),
-                  )
-                ) {
-                  if (label.nodeName === 'DETAILS') {
-                    if (!reset && !label.open) {
-                      label.open = true;
-                      label.dataset.openedByFilter = true;
-                    } else if (reset && label.dataset.openedByFilter) {
-                      label.open = false;
-                      delete label.dataset.openedByFilter;
-                    }
-                  }
-                  showElement(label);
-                } else {
-                  hideElement(label);
-                }
+            function normalizeString(str) {
+              return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
+            }
+
+            const reStartsWith = (query) => new RegExp(`\\b${query}`);
+
+            function searchMethod(target, query) {
+              return (searchStart === 'true')
+                ? target.search(reStartsWith(query)) !== -1
+                : target.includes(query);
+            }
+
+            // Table can be in another context so we have to search in document.
+            const tables = document.querySelectorAll(table);
+
+            const initTable = (tableElement) => {
+
+              // Transitional information helping accelerate queries.
+              let filterItemsTransitional = null;
+              let queryTransitional = "";
+
+              // Prepare items for accelerated filtering.
+              const filterItems = Array.from(
+                tableElement.querySelectorAll(items)
+              ).map((source) => {
+
+                // If we need to search deeper in row elements.
+                const textContent = (targets)
+                  ?
+                    Array.from(
+                      source.querySelectorAll(targets)
+                    ).reduce((acc, target) => {
+                      return acc + ' ' + target.textContent;
+                    }, ' ').trim()
+                  :
+                    source.textContent;
+
+                return {
+                  node: source,
+                  textContent: normalizeString(textContent),
+                };
               });
-            };
 
-            const filterTableList = (e) => {
-              const query = normalizeString(e.target.value);
-              const queryContinuation = query.startsWith(queryTransitional);
+              filterItemsTransitional = filterItems;
 
-              // Return earlier and save resources.
-              // console.warn('transitional recordset is', filterItemsTransitional);
-              if (queryContinuation && filterItemsTransitional.length === 0) {console.log('returning earlier'); return;}
+              const labels = tableElement.querySelectorAll('[data-filter-label]');
 
-              // Test if the updated input text is a continuation of the previous query.
-              if (!searchMethod(query, queryTransitional)) {
-                filterItemsTransitional = filterItems;
-              }
-
-              // Filter if the length of the query is at least 2 characters.
-              if (query.length >= minLength) {
-                let matches = [];
-
-                filterItemsTransitional.forEach((item) => {
-                  if (!searchMethod(item.textContent, query)) {
-                    hideElement(item.node);
-                  } else {
-                    showElement(item.node);
-                    matches.push(item);
-                  }
-                });
-                makeAnnounce(matches.length);
-                checkLabels();
-                filterItemsTransitional = matches;
-              } else {
-                filterItems.forEach((item) => {
-                  showElement(item.node);
-                });
-                Drupal.announce(full || ALL_PHRASE);
-                checkLabels(true);
-                filterItemsTransitional = filterItems
-              }
-
-              // Updates the transitional information.
-              queryTransitional = query;
-            };
-
-            tableElement.addEventListener(FILTER_EVENT, (e) => {
-              return filterTableList(e.detail.event);
-            });
-          };
-
-          tables.forEach(initTable);
-
-          input.addEventListener(
-            'input',
-            debounce((event) => {
-              tables.forEach((tableElement) => {
-                tableElement.dispatchEvent(
-                  new CustomEvent(FILTER_EVENT, {
-                    detail: {
-                      event,
-                    },
-                  }),
+              labels.forEach((label) => {
+                label.labelledItems = tableElement.querySelectorAll(
+                  `[data-filter-labelledby="${label.dataset.filterLabel}"]`,
                 );
               });
-            }, 200),
-          );
 
-          input.addEventListener('keydown', (e) => {
-            if (e.which === 13) {
-              e.preventDefault();
-              e.stopPropagation();
-            }
-          });
+              const checkLabels = (reset = false) => {
+                labels.forEach((label) => {
+                  if (
+                    Array.from(label.labelledItems).some(
+                      (element) => !element.hasAttribute('hidden'),
+                    )
+                  ) {
+                    if (label.nodeName === 'DETAILS') {
+                      if (!reset && !label.open) {
+                        label.open = true;
+                        label.dataset.openedByFilter = true;
+                      } else if (reset && label.dataset.openedByFilter) {
+                        label.open = false;
+                        delete label.dataset.openedByFilter;
+                      }
+                    }
+                    showElement(label);
+                  } else {
+                    hideElement(label);
+                  }
+                });
+              };
+
+              const filterTableList = (e) => {
+                const query = normalizeString(e.target.value);
+                const queryContinuation = query.startsWith(queryTransitional);
+
+                // Return earlier and save resources.
+                // console.warn('transitional recordset is', filterItemsTransitional);
+                if (queryContinuation && filterItemsTransitional.length === 0) {console.log('returning earlier'); return;}
+
+                // Test if the updated input text is a continuation of the previous query.
+                if (!searchMethod(query, queryTransitional)) {
+                  filterItemsTransitional = filterItems;
+                }
+
+                // Filter if the length of the query is at least 2 characters.
+                if (query.length >= minLength) {
+                  let matches = [];
+
+                  filterItemsTransitional.forEach((item) => {
+                    if (!searchMethod(item.textContent, query)) {
+                      hideElement(item.node);
+                    } else {
+                      showElement(item.node);
+                      matches.push(item);
+                    }
+                  });
+                  makeAnnounce(matches.length);
+                  checkLabels();
+                  filterItemsTransitional = matches;
+                } else {
+                  filterItems.forEach((item) => {
+                    showElement(item.node);
+                  });
+                  Drupal.announce(full || ALL_PHRASE);
+                  checkLabels(true);
+                  filterItemsTransitional = filterItems
+                }
+
+                // Updates the transitional information.
+                queryTransitional = query;
+              };
+
+              tableElement.addEventListener(FILTER_EVENT, (e) => {
+                return filterTableList(e.detail.event);
+              });
+            };
+
+            tables.forEach(initTable);
+
+            e.target.addEventListener(
+              'input',
+              debounce((event) => {
+                tables.forEach((tableElement) => {
+                  console.time('timer')
+                  tableElement.dispatchEvent(
+                    new CustomEvent(FILTER_EVENT, {
+                      detail: {
+                        event,
+                      },
+                    }),
+                  );
+                  console.timeEnd('timer')
+                });
+              }, 200),
+            );
+
+            e.target.addEventListener('keydown', (e) => {
+              if (e.which === 13) {
+                e.preventDefault();
+                e.stopPropagation();
+              }
+            });
+          }
+
+          // Activate the input filter only when focused.
+          input.addEventListener('focus', activateFilter, { once: true });
         },
       );
     },
