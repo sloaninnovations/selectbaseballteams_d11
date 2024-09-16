722d3a7784fe8a30b45149c266eb199f7902c2ce
Joachim Noreiko <20313-joachim@users.noreply.drupalcode.org>
2024-09-16 10:12:10 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: Benji Fisher <4949-benjifisher@users.noreply.drupalcode.org>

--- core/modules/user/src/Plugin/migrate/destination/EntityUser.php
+++ core/modules/user/src/Plugin/migrate/destination/EntityUser.php
@@ -119,9 +119,9 @@ class EntityUser extends EntityContentBase {
       array_keys($container->get('entity_type.bundle.info')->getBundleInfo($entity_type)),
       $container->get('entity_field.manager'),
       $container->get('plugin.manager.field.field_type'),
-      $container->get('entity_type.bundle.info'),
       $container->get('password'),
-      $container->get('account_switcher')
+      $container->get('account_switcher'),
+      $container->get('entity_type.bundle.info'),
     );
   }
 
