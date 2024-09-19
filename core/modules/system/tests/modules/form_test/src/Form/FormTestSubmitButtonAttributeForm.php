3b6621f73a5f5efc7bb45f7d51fd240053cde8d8
andrew farquharson <2562-andrewfarq@users.noreply.drupalcode.org>
2024-09-19 13:51:36 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: Stephen Mustgrave <38930-smustgrave@users.noreply.drupalcode.org>

--- core/modules/system/tests/modules/form_test/src/Form/FormTestSubmitButtonAttributeForm.php
+++ core/modules/system/tests/modules/form_test/src/Form/FormTestSubmitButtonAttributeForm.php
@@ -45,7 +45,7 @@ class FormTestSubmitButtonAttributeForm extends FormBase {
   /**
    * {@inheritdoc}
    */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
+  public function submitForm(array &$form, FormStateInterface $form_state): void {
   }
 
 }
