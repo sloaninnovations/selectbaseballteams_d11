13e4ae3403ded4a63ae871e7598fd91e31af95c0
Joachim Noreiko <20313-joachim@users.noreply.drupalcode.org>
2024-09-25 08:34:28 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: Benji Fisher <4949-benjifisher@users.noreply.drupalcode.org>

--- core/modules/migrate/tests/src/Unit/Plugin/migrate/destination/EntityContentBaseTest.php
+++ core/modules/migrate/tests/src/Unit/Plugin/migrate/destination/EntityContentBaseTest.php
@@ -99,7 +99,7 @@ class EntityContentBaseTest extends EntityTestBase {
       $this->entityFieldManager->reveal(),
       $this->prophesize(FieldTypePluginManagerInterface::class)->reveal(),
       $this->prophesize(AccountSwitcherInterface::class)->reveal(),
-      $this->prophesize(EntityTypeBundleInfoInterface::class)->reveal()
+      $this->prophesize(EntityTypeBundleInfoInterface::class)->reveal(),
     );
     $this->expectException(MigrateException::class);
     $this->expectExceptionMessage('The "foo" entity type does not support translations.');
