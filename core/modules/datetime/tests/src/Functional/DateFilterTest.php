c06f985410d00f805b8a083f013fff23a9106d40
andrew farquharson <farquharson.a@gmail.com>
2024-10-07 02:34:50 +0100

    add comment

--- core/modules/datetime/tests/src/Functional/DateFilterTest.php
+++ core/modules/datetime/tests/src/Functional/DateFilterTest.php
@@ -88,11 +88,12 @@ class DateFilterTest extends ViewTestBase {
     $this->drupalGet('test_exposed_filter_datetime');
     $this->assertSession()->statusCodeEquals(200);
 
+    // Ensure that invalid date format entries in the exposed filter are validated
     $edit = ['edit-field-date-value-value' => 'lun 2018-04-27'];
     $this->submitForm($edit, 'Apply');
     $this->assertSession()->pageTextContains('Invalid date format.');
     $this->assertSession()->pageTextNotContains('Exception: DateTime object not set.');
-    
+
     $this->assertSession()->optionExists('edit-field-date-value-op', '=');
     $this->assertSession()->optionNotExists('edit-field-date-value-op', '>');
     $this->assertSession()->optionNotExists('edit-field-date-value-op', '>=');
