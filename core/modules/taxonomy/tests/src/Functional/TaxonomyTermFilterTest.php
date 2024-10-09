b6ada1e1c4f5ae168f29fef5a36ad2ee63ce43f8
Víctor Dorado <victor.dorado.asensio@gmail.com>
2024-10-09 16:41:09 +0200

    Add a forgotten {@inheritdoc}

--- core/modules/taxonomy/tests/src/Functional/TaxonomyTermFilterTest.php
+++ core/modules/taxonomy/tests/src/Functional/TaxonomyTermFilterTest.php
@@ -9,6 +9,8 @@ use Drupal\taxonomy\VocabularyInterface;
 /**
  * Ensure that the term overview page filtering works properly.
  *
+ * {@inheritdoc}
+ *
  * @group taxonomy
  */
 class TaxonomyTermFilterTest extends TaxonomyTestBase {
