25ab4be25d917174cbefad690cd7b1f266474504
Michael Strelan <mstrelan@gmail.com>
2024-10-14 08:33:14 +1000

    Fix phpcs

--- core/modules/taxonomy/tests/src/Functional/TermIndexTest.php
+++ core/modules/taxonomy/tests/src/Functional/TermIndexTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Drupal\Tests\taxonomy\Functional;
 
-use Drupal\Core\Link;
 use Drupal\Core\Database\Database;
 use Drupal\Core\Field\FieldStorageDefinitionInterface;
 
