e9ad41661a649cbcfb598d595da0422deef81666
mondrake <mondrake.org@gmail.com>
2024-10-26 16:13:58 +0200

    fixes

--- core/tests/Drupal/Tests/Core/Database/UrlConversionTest.php
+++ core/tests/Drupal/Tests/Core/Database/UrlConversionTest.php
@@ -42,7 +42,6 @@ class UrlConversionTest extends UnitTestCase {
    *   - database_array: An array containing the expected results.
    */
   public static function providerConvertDbUrlToConnectionInfo() {
-    $root = dirname(__FILE__, 7);
     return [
       'MySql without prefix' => [
         'mysql://test_user:test_pass@test_host:3306/test_database',
@@ -264,7 +263,7 @@ class UrlConversionTest extends UnitTestCase {
         [
           'driver' => 'sqlite',
           'host' => 'localhost',
-          'database' => $root . '/test_database',
+          'database' => 'test_database',
           'namespace' => 'Drupal\sqlite\Driver\Database\sqlite',
           'autoload' => 'core/modules/sqlite/src/Driver/Database/sqlite/',
         ],
