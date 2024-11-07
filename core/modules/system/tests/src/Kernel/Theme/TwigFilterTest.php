f6267c3e3efa4f805ea464303cdcab9ad2001b02
Edward Wu <godotislate@gmail.com>
2024-11-07 07:54:09 -0800

    Add test for spaceless use.

--- core/modules/system/tests/src/Kernel/Theme/TwigFilterTest.php
+++ core/modules/system/tests/src/Kernel/Theme/TwigFilterTest.php
@@ -136,4 +136,41 @@ class TwigFilterTest extends KernelTestBase {
     }
   }
 
+  /**
+   * Tests Twig "spaceless" filter.
+   *
+   * The "spaceless" filter was deprecated in Twig 3.12. This is a test for the
+   * replacement spaceless filter provided by Drupal's Twig environment for
+   * backwards compatibility.
+   */
+  public function testSpacelessFilter(): void {
+    /** @var \Drupal\Core\Render\RendererInterface $renderer */
+    $renderer = $this->container->get('renderer');
+
+    $element = [
+      '#type' => 'inline_template',
+      '#template' => <<<'EOT'
+{% apply spaceless %}
+<div>
+  <span>Use the spaceless filter to remove whitespace between HTML tags, not whitespace within HTML tags or whitespace in plain text.</span>
+</div>
+{% endapply %}
+EOT,
+    ];
+    $this->assertSame('<div><span>Use the spaceless filter to remove whitespace between HTML tags, not whitespace within HTML tags or whitespace in plain text.</span></div>', (string) $renderer->renderRoot($element));
+
+    // Also test drupal_spaceless filter directly.
+    $element = [
+      '#type' => 'inline_template',
+      '#template' => <<<'EOT'
+{% apply drupal_spaceless %}
+<div>
+  <span>Use the drupal_spaceless filter to remove whitespace between HTML tags, not whitespace within HTML tags or whitespace in plain text.</span>
+</div>
+{% endapply %}
+EOT,
+    ];
+    $this->assertSame('<div><span>Use the drupal_spaceless filter to remove whitespace between HTML tags, not whitespace within HTML tags or whitespace in plain text.</span></div>', (string) $renderer->renderRoot($element));
+  }
+
 }
