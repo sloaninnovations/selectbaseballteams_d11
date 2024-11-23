beb0d7c3739b1ef14527654f3771f5c00c533da5
Edward Wu <godotislate@gmail.com>
2024-11-23 09:08:17 -0800

    Fixes from code review.

--- core/modules/image/src/Plugin/Field/FieldFormatter/ImageFormatterBase.php
+++ core/modules/image/src/Plugin/Field/FieldFormatter/ImageFormatterBase.php
@@ -38,7 +38,7 @@ abstract class ImageFormatterBase extends FileFormatterBase {
           '_loaded' => TRUE,
           '_is_default' => TRUE,
         ]);
-        if (isset($file->_referringItem)) {
+        if ($file->_referringItem) {
           // If the file entity is already being referenced by another field
           // item, clone it so that _referringItem is set to the correct item
           // in each instance.
