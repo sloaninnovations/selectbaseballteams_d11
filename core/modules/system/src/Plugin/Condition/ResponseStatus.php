2cf0ec96a2056b2b208177a48ff6181cc2bda5ec
quietone <quietone@2572884.no-reply.drupal.org>
2024-11-24 18:18:56 +1300

    a start

--- core/modules/system/src/Plugin/Condition/ResponseStatus.php
+++ core/modules/system/src/Plugin/Condition/ResponseStatus.php
@@ -38,6 +38,9 @@ class ResponseStatus extends ConditionPluginBase implements ContainerFactoryPlug
     return $instance;
   }
 
+  /**
+   *  Sets the request stack.
+   */
   public function setRequestStack(RequestStack $requestStack): void {
     $this->requestStack = $requestStack;
   }
