c6cabfa492826f8734d7f4727881467721c13554
nlighteneddesign <nic@nlighteneddevelopment.com>
2024-11-27 08:58:04 -0500

    Remove content_moderation helper

--- core/modules/content_moderation/src/Hook/ContentModerationViewsHooks.php
+++ core/modules/content_moderation/src/Hook/ContentModerationViewsHooks.php
@@ -2,19 +2,31 @@
 
 namespace Drupal\content_moderation\Hook;
 
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Hook\Attribute\Hook;
+use Drupal\content_moderation\ModerationInformation;
+use Drupal\content_moderation\ViewsData;
 
 /**
  * Hook implementations for content_moderation.
  */
 class ContentModerationViewsHooks {
 
+  public function __construct(
+    private readonly EntityTypeManagerInterface $entityTypeManager,
+    private readonly ModerationInformation $moderationInformation,
+  ) {}
+
   /**
    * Implements hook_views_data().
    */
   #[Hook('views_data')]
   public function viewsData(): array {
-    return _content_moderation_views_data_object()->getViewsData();
+    $viewsData = new ViewsData(
+      $this->entityTypeManager,
+      $this->moderationInformation
+    );
+    return $viewsData->getViewsData();
   }
 
 }
