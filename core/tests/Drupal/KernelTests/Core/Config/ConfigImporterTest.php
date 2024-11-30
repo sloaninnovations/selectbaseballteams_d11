882a4beb094c7fca355623ed08e39ee80d25842b
Nikolay Shapovalov <zniki.ru@1877422.no-reply.drupal.org>
2024-11-30 13:29:10 +0300

    remove FormattableMarkup

--- core/tests/Drupal/KernelTests/Core/Config/ConfigImporterTest.php
+++ core/tests/Drupal/KernelTests/Core/Config/ConfigImporterTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Drupal\KernelTests\Core\Config;
 
 use Drupal\Component\Utility\Html;
-use Drupal\Component\Render\FormattableMarkup;
 use Drupal\Core\Config\ConfigCollectionEvents;
 use Drupal\Core\Config\ConfigEvents;
 use Drupal\Core\Config\ConfigImporter;
@@ -234,7 +233,7 @@ class ConfigImporterTest extends KernelTestBase {
 
     $logs = $config_importer->getErrors();
     $this->assertCount(1, $logs);
-    $this->assertEquals(new FormattableMarkup('Deleted and replaced configuration entity "@name"', ['@name' => $name_secondary]), $logs[0]);
+    $this->assertEquals(sprintf('Deleted and replaced configuration entity "%s"', $name_secondary), $logs[0]);
   }
 
   /**
@@ -364,7 +363,7 @@ class ConfigImporterTest extends KernelTestBase {
 
     $logs = $config_importer->getErrors();
     $this->assertCount(1, $logs);
-    $this->assertEquals(new FormattableMarkup('Update target "@name" is missing.', ['@name' => $name_dependent]), $logs[0]);
+    $this->assertEquals(sprintf('Update target "%s" is missing.', $name_dependent), $logs[0]);
   }
 
   /**
