71d4cfed0a11cebda33b5438e3deb2c97e55c13b
Nikolay Shapovalov <zniki.ru@1877422.no-reply.drupal.org>
2024-11-30 13:42:12 +0300

    remove FormattableMarkup

--- core/tests/Drupal/KernelTests/Core/Entity/EntityAccessControlHandlerTest.php
+++ core/tests/Drupal/KernelTests/Core/Entity/EntityAccessControlHandlerTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Drupal\KernelTests\Core\Entity;
 
-use Drupal\Component\Render\FormattableMarkup;
 use Drupal\Core\Language\LanguageInterface;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Access\AccessibleInterface;
@@ -45,10 +44,8 @@ class EntityAccessControlHandlerTest extends EntityLanguageTestBase {
    */
   public function assertEntityAccess(array $ops, AccessibleInterface $object, ?AccountInterface $account = NULL): void {
     foreach ($ops as $op => $result) {
-      $message = new FormattableMarkup("Entity access returns @result with operation '@op'.", [
-        '@result' => !isset($result) ? 'null' : ($result ? 'true' : 'false'),
-        '@op' => $op,
-      ]);
+      $access_result = !isset($result) ? 'null' : ($result ? 'true' : 'false');
+      $message = sprintf("Entity access returns %s with operation '%s'.", $access_result, $op);
 
       $this->assertEquals($object->access($op, $account), $result, (string) $message);
     }
