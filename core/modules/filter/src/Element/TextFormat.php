af462a237880c21ed766804fa620c1f071443fa9
Lavanya <talwarlavanya2002@gmail.com>
2024-12-03 13:06:49 +0530

    #Issue:3489416-Added return comments to some files.

--- core/modules/filter/src/Element/TextFormat.php
+++ core/modules/filter/src/Element/TextFormat.php
@@ -295,6 +295,7 @@ class TextFormat extends RenderElementBase {
    * Wraps the config factory.
    *
    * @return \Drupal\Core\Config\ConfigFactoryInterface
+   *   The configuration factory service.
    */
   protected static function configFactory() {
     return \Drupal::configFactory();
@@ -304,6 +305,7 @@ class TextFormat extends RenderElementBase {
    * Wraps the element info service.
    *
    * @return \Drupal\Core\Render\ElementInfoManagerInterface
+   *   The element info service.
    */
   protected static function elementInfo() {
     return \Drupal::service('element_info');
