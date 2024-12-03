b568420fbd9c761df598f8d07d2320f8c46244b0
nicxvan <nic@nlightened.net>
2024-12-02 22:00:21 -0500

    Test alter

--- core/modules/system/tests/modules/module_runtime_requirements/src/Hook/ModuleRuntimeRequirementsHooks.php
+++ core/modules/system/tests/modules/module_runtime_requirements/src/Hook/ModuleRuntimeRequirementsHooks.php
@@ -23,6 +23,25 @@ class ModuleRuntimeRequirementsHooks {
         'description' => t("Runtime Error."),
         'severity' => REQUIREMENT_ERROR,
       ],
+      'test.runtime.error.alter' => [
+        'title' => t('RuntimeError'),
+        'value' => t('None'),
+        'description' => t("Runtime Error."),
+        'severity' => REQUIREMENT_ERROR,
+      ],
+    ];
+  }
+
+  /**
+   * Implements hook_runtime_requirements_alter().
+   */
+  #[Hook('runtime_requirements_alter')]
+  public function runtimeRequirementsAlter(array &$requirements): void {
+    $requirements['test.runtime.error.alter'] = [
+      'title' => t('RuntimeWarning'),
+      'value' => t('None'),
+      'description' => t("Runtime Warning."),
+      'severity' => REQUIREMENT_WARNING,
     ];
   }
 
