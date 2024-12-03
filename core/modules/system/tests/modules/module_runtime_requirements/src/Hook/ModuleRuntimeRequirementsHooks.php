096fe67a4618006d2cfeffdf5514c2e3d7a60485
nicxvan <nic@nlightened.net>
2024-12-02 21:19:19 -0500

    Test hook runtime requirements

---
+++ core/modules/system/tests/modules/module_runtime_requirements/src/Hook/ModuleRuntimeRequirementsHooks.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\module_runtime_requirements\Hook;
+
+use Drupal\Core\Hook\Attribute\Hook;
+
+/**
+ * Hook implementations for module_runtime_requirements.
+ */
+class ModuleRuntimeRequirementsHooks {
+
+  /**
+   * Implements hook_runtime_requirements().
+   *
+   * Manipulate module dependencies to test dependency chains.
+   */
+  #[Hook('runtime_requirements')]
+  public function runtimeRequirements(): array {
+    return [
+      'test.runtime.error' => [
+        'title' => t('RuntimeError'),
+        'value' => t('None'),
+        'description' => t("Runtime Error."),
+        'severity' => REQUIREMENT_ERROR,
+      ],
+    ];
+  }
+
+}
