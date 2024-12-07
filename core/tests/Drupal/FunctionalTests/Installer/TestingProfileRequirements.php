00fc8a3cf5ebf87254b6123d2be6b66f092564e9
nicxvan <nic@nlightened.net>
2024-12-07 14:50:58 -0500

    unused

--- core/tests/Drupal/FunctionalTests/Installer/TestingProfileRequirements.php
+++ core/tests/Drupal/FunctionalTests/Installer/TestingProfileRequirements.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Drupal\FunctionalTests\Installer;
 
-use Drupal\FunctionalTests\Installer\InstallerTestBase;
-
 /**
  * Tests installing a profile that implements InstallRequirementsInterface.
  *
