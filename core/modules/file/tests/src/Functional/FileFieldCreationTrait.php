8eff9e5a2d5ca068b678ec188446d035a5b9a9e1
Mohit Aghera <mohit.beta@gmail.com>
2024-12-07 12:25:50 +0530

    Linting fixes.

--- core/modules/file/tests/src/Functional/FileFieldCreationTrait.php
+++ core/modules/file/tests/src/Functional/FileFieldCreationTrait.php
@@ -6,6 +6,7 @@ namespace Drupal\Tests\file\Functional;
 
 use Drupal\field\Entity\FieldStorageConfig;
 use Drupal\field\Entity\FieldConfig;
+use Drupal\field\FieldStorageConfigInterface;
 
 /**
  * Provides methods for creating file fields.
@@ -32,7 +33,7 @@ trait FileFieldCreationTrait {
    * @return \Drupal\field\FieldStorageConfigInterface
    *   The file field.
    */
-  protected function createFileField($name, $entity_type, $bundle, $storage_settings = [], $field_settings = [], $widget_settings = []) {
+  protected function createFileField($name, $entity_type, $bundle, $storage_settings = [], $field_settings = [], $widget_settings = []): FieldStorageConfigInterface {
     $field_storage = FieldStorageConfig::create([
       'entity_type' => $entity_type,
       'field_name' => $name,
@@ -61,7 +62,7 @@ trait FileFieldCreationTrait {
    * @param array $widget_settings
    *   A list of widget settings that will be added to the widget defaults.
    */
-  protected function attachFileField($name, $entity_type, $bundle, $field_settings = [], $widget_settings = []) {
+  protected function attachFileField($name, $entity_type, $bundle, $field_settings = [], $widget_settings = []): void {
     $field = [
       'field_name' => $name,
       'label' => $name,
