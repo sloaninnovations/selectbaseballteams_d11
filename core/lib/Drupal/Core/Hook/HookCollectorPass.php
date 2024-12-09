4f7f2fffd36cfe9da7ed6eca2f66153921e31b0f
nlighteneddesign <nic@nlighteneddevelopment.com>
2024-12-09 13:00:54 -0500

    Add change priority and grouping WIP

--- core/lib/Drupal/Core/Hook/HookCollectorPass.php
+++ core/lib/Drupal/Core/Hook/HookCollectorPass.php
@@ -72,6 +72,11 @@ class HookCollectorPass implements CompilerPassInterface {
   private array $groupIncludes = [];
 
   /**
+   * A list of implementations to reprioritize.
+   */
+  protected array $orderGroup = [];
+
+  /**
    * {@inheritdoc}
    */
   public function process(ContainerBuilder $container): void {
@@ -309,6 +314,14 @@ class HookCollectorPass implements CompilerPassInterface {
     }
     $this->moduleImplements[$hook->hook][$module] = '';
     $this->implementations[$hook->hook][$module][$class][] = $hook->method;
+
+    if ($hook->orderGroup) {
+      if (!isset($this->orderGroup[$hook->orderGroup])) {
+        $this->orderGroup[$hook->orderGroup] = ['drupal_hook' . $hook->orderGroup];
+      }
+      $this->orderGroup[$hook->orderGroup][$hook->hook] = ['drupal_hook' . $hook->hook];
+
+    }
   }
 
   /**
@@ -380,6 +393,113 @@ class HookCollectorPass implements CompilerPassInterface {
     if (in_array($hook->hook, $staticDenyHooks) || preg_match('/^(post_update_|preprocess_|update_\d+$)/', $hook->hook)) {
       throw new \LogicException("The hook $hook->hook on class $class does not support attributes and must remain procedural.");
     }
+  }
+
+    /**
+   * Change the priority of a hook implementation.
+   *
+   * @param \Drupal\Core\DependencyInjection\ContainerBuilder $container
+   *   The container builder.
+   * @param string $hook
+   *   The name of the hook.
+   * @param string $class_and_method
+   *   Class and method separated by :: containing the hook implementation which
+   *   should be changed.
+   * @param bool $should_be_larger
+   *   TRUE for before/first, FALSE for after/last. Larger priority listeners
+   *   fire first.
+   * @param array|null $others
+   *   Other hook implementations to compare to, if any. The array is keyed by
+   *   string containing a class and method separated by ::, the value is not
+   *   used.
+   *
+   * @return void
+   */
+  protected function changePriority(ContainerBuilder $container, string $hook, string $class_and_method, bool $should_be_larger, ?array $others = NULL): void {
+    $events = $this->orderGroup[$hook] ?? ["drupal_hook.$hook"];
+    foreach ($container->findTaggedServiceIds('kernel.event_listener') as $id => $attributes) {
+      foreach ($attributes as $key => $tag) {
+        if (in_array($tag['event'], $events)) {
+          $index = "$id.$key";
+          $priority = $tag['priority'];
+          // Symfony documents event listener priorities to be integers,
+          // HookCollectorPass sets them to be integers, ::setPriority() only
+          // accepts integers.
+          assert(is_int($priority));
+          $priorities[$index] = $priority;
+          $specifier = "$id::" . $tag['method'];
+          if ($class_and_method === $specifier) {
+            $index_this = $index;
+          }
+          // If $others is specified by ::before() / ::after() then for
+          // comparison only the priority of those matter.
+          // For ::first() / ::last() the priority of every other hook
+          // matters.
+          elseif (!isset($others) || isset($others[$specifier])) {
+            $priorities_other[] = $priority;
+          }
+        }
+      }
+    }
+    if (!isset($index_this) || !isset($priorities) || !isset($priorities_other)) {
+      return;
+    }
+    // The priority of the hook being changed.
+    $priority_this = $priorities[$index_this];
+    // The priority of the hook being compared to.
+    $priority_other = $should_be_larger ? max($priorities_other) : min($priorities_other);
+    // If the order is correct there is nothing to do. If the two priorities
+    // are the same then the order is undefined and so it can't be correct.
+    // If they are not the same and $priority_this is already larger exactly
+    // when $should_be_larger says then it's the correct order.
+    if ($priority_this !== $priority_other && ($should_be_larger === ($priority_this > $priority_other))) {
+      return;
+    }
+    $priority_new = $priority_other + ($should_be_larger ? 1 : -1);
+    // For ::first() / ::last() this new priority is already larger/smaller
+    // than all existing priorities but for ::before() / ::after() it might
+    // belong to an already existing hook. In this case set the new priority
+    // temporarily to be halfway between $priority_other and $priority_new
+    // then give all hook implementations new, integer priorities keeping this
+    // new order. This ensures the hook implementation being changed is in the
+    // right order relative to both $priority_other and the hook whose
+    // priority was $priority_new.
+    if (in_array($priority_new, $priorities)) {
+      $priorities[$index_this] = $priority_other + ($should_be_larger ? 0.5 : -0.5);
+      asort($priorities);
+      $changed_indexes = array_keys($priorities);
+      $priorities = array_combine($changed_indexes, range(1, count($changed_indexes)));
+    }
+    else {
+      $priorities[$index_this] = $priority_new;
+      $changed_indexes = [$index_this];
+    }
+    foreach ($changed_indexes as $index) {
+      [$id, $key] = explode('.', $index);
+      self::setPriority($container, $id, (int) $key, $priorities[$index]);
+    }
+  }
+
+  /**
+   * Set the priority of a listener.
+   *
+   * @param \Drupal\Core\DependencyInjection\ContainerBuilder $container
+   *   The container.
+   * @param string $class
+   *   The name of the class, this is the same as the service id.
+   * @param int $key
+   *   The key within the tags array of the 'kernel.event_listener' tag for the
+   *   hook implementation to be changed.
+   * @param int $priority
+   *   The new priority.
+   *
+   * @return void
+   */
+  public static function setPriority(ContainerBuilder $container, string $class, int $key, int $priority): void {
+    $definition = $container->getDefinition($class);
+    $tags = $definition->getTags();
+    $tags['kernel.event_listener'][$key]['priority'] = $priority;
+    $definition->setTags($tags);
   }
 
 }
