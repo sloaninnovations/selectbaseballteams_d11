ab1efb77f1e8fb82ee694b2df78790b49e3f2e94
nlighteneddesign <nic@nlighteneddevelopment.com>
2024-12-13 10:34:57 -0500

    Convert hook_requirements

---
+++ core/modules/file/src/Hook/FileRuntimeRequirements.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Drupal\file\Hook;
+
+use Drupal\Core\Hook\Attribute\Hook;
+
+/**
+ * RuntimeRequirements for file.
+ */
+class FileRuntimeRequirements {
+
+  /**
+   * Implements hook_runtime_requirements().
+   */
+  #[Hook('runtime_requirements')]
+  public function runtime() {
+    $requirements = [];
+    $server_software = \Drupal::request()->server->get('SERVER_SOFTWARE', '');
+
+    // Get the web server identity.
+    $is_nginx = preg_match("/Nginx/i", $server_software);
+    $is_apache = preg_match("/Apache/i", $server_software);
+    $fastcgi = $is_apache && ((str_contains($server_software, 'mod_fastcgi') || str_contains($server_software, 'mod_fcgi')));
+
+    // Check the uploadprogress extension is loaded.
+    if (extension_loaded('uploadprogress')) {
+      $value = t('Enabled (<a href="http://pecl.php.net/package/uploadprogress">PECL uploadprogress</a>)');
+      $description = NULL;
+    }
+    else {
+      $value = t('Not enabled');
+      $description = t('Your server is capable of displaying file upload progress, but does not have the required libraries. It is recommended to install the <a href="http://pecl.php.net/package/uploadprogress">PECL uploadprogress library</a>.');
+    }
+
+    // Adjust the requirement depending on what the server supports.
+    if (!$is_apache && !$is_nginx) {
+      $value = t('Not enabled');
+      $description = t('Your server is not capable of displaying file upload progress. File upload progress requires an Apache server running PHP with mod_php or Nginx with PHP-FPM.');
+    }
+    elseif ($fastcgi) {
+      $value = t('Not enabled');
+      $description = t('Your server is not capable of displaying file upload progress. File upload progress requires PHP be run with mod_php or PHP-FPM and not as FastCGI.');
+    }
+
+    $requirements['file_progress'] = [
+      'title' => t('Upload progress'),
+      'value' => $value,
+      'description' => $description,
+    ];
+
+    return $requirements;
+  }
+
+}
