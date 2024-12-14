9521c038617f2d312d94c1c7f4285c2f068f72cb
nlighteneddesign <nic@nlighteneddevelopment.com>
2024-12-13 22:28:03 -0500

    Add comment to test modules

--- core/modules/system/tests/modules/hook_order_first_alphabetically2/src/Hook/FirstAlphabeticallyHooks2.php
+++ core/modules/system/tests/modules/hook_order_first_alphabetically2/src/Hook/FirstAlphabeticallyHooks2.php
@@ -8,7 +8,18 @@ use Drupal\Core\Hook\Attribute\Hook;
 use Drupal\Core\Hook\Attribute\HookAfter;
 
 /**
- * Hook implementations for hook_order_first_alphabetically.
+ * Hook implementations for verifying ordering hooks by attributes.
+ *
+ * We must ensure that the order of the modules is expected and then change
+ * the order that the hooks are run in order to verify. All of these modules
+ * come in a pair first alphabetically and last alphabetically.
+ *
+ * In the normal order a hook implemented by first alphabetically would run
+ * before the same hook in last alphabetically.
+ *
+ * Each pair tests one hook order attribute.
+ *
+ * This pair tests #[HookAfter].
  */
 class FirstAlphabeticallyHooks2 {
 
--- core/modules/system/tests/modules/hook_order_last_alphabetically2/src/Hook/LastAlphabeticallyHooks2.php
+++ core/modules/system/tests/modules/hook_order_last_alphabetically2/src/Hook/LastAlphabeticallyHooks2.php
@@ -7,7 +7,18 @@ namespace Drupal\hook_order_last_alphabetically2\Hook;
 use Drupal\Core\Hook\Attribute\Hook;
 
 /**
- * Hook implementations for hook_order_last_alphabetically.
+ * Hook implementations for verifying ordering hooks by attributes.
+ *
+ * We must ensure that the order of the modules is expected and then change
+ * the order that the hooks are run in order to verify. All of these modules
+ * come in a pair first alphabetically and last alphabetically.
+ *
+ * In the normal order a hook implemented by first alphabetically would run
+ * before the same hook in last alphabetically.
+ *
+ * Each pair tests one hook order attribute.
+ *
+ * This pair tests #[HookAfter].
  */
 class LastAlphabeticallyHooks2 {
 
