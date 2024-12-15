56fa93f62cce26443615fff62943eb23d4502cdb
nlighteneddesign <nic@nlighteneddevelopment.com>
2024-12-14 20:38:54 -0500

    Unified approach

---
+++ core/lib/Drupal/Core/Hook/Attribute/Order.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Core\Hook\Attribute;
+
+class Order {
+
+  public function __construct(
+    public ?OrderType $type,
+    public ?array $modules = [],
+    public ?array $classesAndMethods = [],
+    public ?array $group = NULL,
+  ) {
+    if (!$this->modules && !$this->classesAndMethods) {
+      throw new \LogicException('Order against what?');
+    }
+  }
+
+}
