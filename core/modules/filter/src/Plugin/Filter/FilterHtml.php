c516f84d327ab1dbcdbe6db427122f91ed5b98b3
Lavanya <talwarlavanya2002@gmail.com>
2024-12-16 12:09:26 +0530

    #Issue:3489416-Fixed PHP static analysis error.

--- core/modules/filter/src/Plugin/Filter/FilterHtml.php
+++ core/modules/filter/src/Plugin/Filter/FilterHtml.php
@@ -267,7 +267,7 @@ class FilterHtml extends FilterBase {
     $scanner = new Scanner('<body>' . $html);
     $parser = new class($scanner, $events) extends Tokenizer {
 
-      public function setTextMode($textMode, $untilTag = NULL) {
+      public function setTextMode($textMode, $untilTag = NULL): void {
         // Do nothing, we never enter text mode.
       }
 
