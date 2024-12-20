30e172eb639a1e1c2d41d68360c6776710c66b4d
Christopher Lewis <christopher.lewis@brandcode.de>
2024-12-20 11:10:59 +0100

    Update deprecation notice; mark for removal in 12.0

--- core/modules/datetime_range/src/DateTimeRangeConstantsInterface.php
+++ core/modules/datetime_range/src/DateTimeRangeConstantsInterface.php
@@ -5,7 +5,7 @@ namespace Drupal\datetime_range;
 /**
  * Declares constants used in the datetime range module.
  *
- * @deprecated in drupal:11.0.0 and is removed from drupal:11.1.0. Use
+ * @deprecated in drupal:11.2.0 and is removed from drupal:12.0.0. Use
  *    enum DateTimeRangeDisplayOptions instead.
  * @see https://www.drupal.org/project/drupal/issues/3425141
  */
