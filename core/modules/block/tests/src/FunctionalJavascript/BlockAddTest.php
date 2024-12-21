a96751c7d6095fce8d5f5f66f91389a6493aa082
andrew farquharson <2562-andrewfarq@users.noreply.drupalcode.org>
2024-12-21 04:58:45 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: quietone <33605-quietone@users.noreply.drupalcode.org>

--- core/modules/block/tests/src/FunctionalJavascript/BlockAddTest.php
+++ core/modules/block/tests/src/FunctionalJavascript/BlockAddTest.php
@@ -47,7 +47,7 @@ class BlockAddTest extends WebDriverTestBase {
     $assert_session->assertWaitOnAjaxRequest();
     $assert_session->pageTextNotContains('The submitted value Pre-content in the Region element is not allowed.');
     $assert_session->optionExists('Region', '- Select -');
-    // Check whether the text "Response status" or "Not restricted" is present in the tab titles.
+    // Check that the summary line is not present in the title.
     $assert_session->elementTextNotContains('css', '.vertical-tabs__menu-item-title', 'Response status');
     $assert_session->elementTextNotContains('css', '.vertical-tabs__menu-item-title', 'Not restricted');
 
