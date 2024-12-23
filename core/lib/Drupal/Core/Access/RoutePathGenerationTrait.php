3eb96fc0550dea31e89b48350d94ffc088ce05b7
Víctor Dorado <victor.dorado.asensio@gmail.com>
2024-12-23 13:01:45 +0100

    Improve method documentation.

--- core/lib/Drupal/Core/Access/RoutePathGenerationTrait.php
+++ core/lib/Drupal/Core/Access/RoutePathGenerationTrait.php
@@ -10,29 +10,41 @@ use Symfony\Component\Routing\Route;
 trait RoutePathGenerationTrait {
 
   /**
-   * Performs parameter placeholder replacements on a route path, given an array of parameters.
+   * Generates a route path by replacing parameter placeholders with their values.
+   *
+   * This method replaces placeholders in the given route path using the provided
+   * array of parameters. Placeholders without corresponding values in the array
+   * are removed from the resulting path.
    *
    * @param \Symfony\Component\Routing\Route $route
-   *   The route.
+   *   The route object containing the path with placeholders.
    * @param array $parameters
-   *   The parameters to substitute on the route path.
+   *   An associative array of parameters to replace in the route path.
+   *   Example:
+   *   [
+   *     'parameter-placeholder' => 'parameter-value',
+   *   ]
+   *   This will transform a route path like '/route/path/{parameter-placeholder}'
+   *   into '/route/path/parameter-value'.
    *
    * @return string
+   *   The generated path with all placeholders replaced by their corresponding
+   *   values or removed if no matching parameter exists.
    */
   public function generateRoutePath(Route $route, array $parameters): string {
     $path = ltrim($route->getPath(), '/');
 
-    // Replace the path parameters with values from the parameters array.
+    // Replace path parameters with their corresponding values from the parameters array.
     foreach ($parameters as $param => $value) {
       if (NULL !== $value) {
         $path = str_replace("{{$param}}", $value, $path);
       }
     }
 
-    // Remove not replaced params.
+    // Remove placeholders that were not replaced.
     $path = preg_replace('/\/{[^}]+}/', '', $path);
 
-    // Remove trailing slash.
+    // Remove trailing slashes (multiple slashes may result from the removal of unreplaced placeholders).
     $path = rtrim($path, '/');
 
     return $path;
