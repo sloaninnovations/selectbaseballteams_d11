04b8c3a228c18a468a12dfda80e3873d02303394
Víctor Dorado <victor.dorado.asensio@gmail.com>
2024-12-23 18:47:35 +0100

    Improve documentation

--- core/modules/editor/src/Hook/EditorHooks.php
+++ core/modules/editor/src/Hook/EditorHooks.php
@@ -240,13 +240,14 @@ class EditorHooks {
     if (!$entity instanceof FieldableEntityInterface) {
       return;
     }
-    // Before delete entity usage, delete its translation usage.
+    // Before delete entity usages, delete its translation usages.
     if (count($entity->getTranslationLanguages()) > 1) {
       $languages = $entity->getTranslationLanguages();
       $default_langcode = $entity->language()->getId();
       foreach ($languages as $langcode => $languages) {
         if ($langcode != $default_langcode) {
           $translation = $entity->getTranslation($langcode);
+          // Delete translation paragraphs usages.
           $reference_revisions_entities = _editor_get_entity_reference_revisions($translation);
           foreach ($reference_revisions_entities as $reference_revisions_entity) {
             if ($reference_revisions_entity instanceof EntityInterface) {
@@ -256,6 +257,7 @@ class EditorHooks {
               }
             }
           }
+          // Delete translation usages.
           if ($translation instanceof EntityInterface) {
             $referenced_files_by_field = _editor_get_file_uuids_by_field($translation);
             foreach ($referenced_files_by_field as $uuids) {
@@ -265,6 +267,7 @@ class EditorHooks {
         }
       }
     }
+    // Delete entity paragraphs usages.
     $reference_revisions_entities = _editor_get_entity_reference_revisions($entity);
     foreach ($reference_revisions_entities as $reference_revisions_entity) {
       if ($reference_revisions_entity instanceof EntityInterface) {
@@ -274,7 +277,7 @@ class EditorHooks {
         }
       }
     }
-    // Delete entity usage.
+    // Delete entity usages.
     $referenced_files_by_field = _editor_get_file_uuids_by_field($entity);
     foreach ($referenced_files_by_field as $uuids) {
       _editor_delete_file_usage($uuids, $entity, 0);
