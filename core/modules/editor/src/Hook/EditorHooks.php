4f9e780dec855753bcbd109e3063a815c13f792d
Víctor Dorado <victor.dorado.asensio@gmail.com>
2024-12-23 18:21:32 +0100

    Refactor entity CRUD hooks to follow the new Hook Class approach

--- core/modules/editor/src/Hook/EditorHooks.php
+++ core/modules/editor/src/Hook/EditorHooks.php
@@ -240,6 +240,41 @@ class EditorHooks {
     if (!$entity instanceof FieldableEntityInterface) {
       return;
     }
+    // Before delete entity usage, delete its translation usage.
+    if (count($entity->getTranslationLanguages()) > 1) {
+      $languages = $entity->getTranslationLanguages();
+      $default_langcode = $entity->language()->getId();
+      foreach ($languages as $langcode => $languages) {
+        if ($langcode != $default_langcode) {
+          $translation = $entity->getTranslation($langcode);
+          $reference_revisions_entities = _editor_get_entity_reference_revisions($translation);
+          foreach ($reference_revisions_entities as $reference_revisions_entity) {
+            if ($reference_revisions_entity instanceof EntityInterface) {
+              $referenced_files_by_field = _editor_get_file_uuids_by_field($reference_revisions_entity);
+              foreach ($referenced_files_by_field as $uuids) {
+                _editor_delete_file_usage($uuids, $reference_revisions_entity, 0);
+              }
+            }
+          }
+          if ($translation instanceof EntityInterface) {
+            $referenced_files_by_field = _editor_get_file_uuids_by_field($translation);
+            foreach ($referenced_files_by_field as $uuids) {
+              _editor_delete_file_usage($uuids, $translation, 0);
+            }
+          }
+        }
+      }
+    }
+    $reference_revisions_entities = _editor_get_entity_reference_revisions($entity);
+    foreach ($reference_revisions_entities as $reference_revisions_entity) {
+      if ($reference_revisions_entity instanceof EntityInterface) {
+        $referenced_files_by_field = _editor_get_file_uuids_by_field($reference_revisions_entity);
+        foreach ($referenced_files_by_field as $uuids) {
+          _editor_delete_file_usage($uuids, $reference_revisions_entity, 0);
+        }
+      }
+    }
+    // Delete entity usage.
     $referenced_files_by_field = _editor_get_file_uuids_by_field($entity);
     foreach ($referenced_files_by_field as $uuids) {
       _editor_delete_file_usage($uuids, $entity, 0);
