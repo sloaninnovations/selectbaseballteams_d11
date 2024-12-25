7582ae01c5fb5c303edca3492b40d98141f7d0b6
Nikolay Shapovalov <zniki.ru@1877422.no-reply.drupal.org>
2024-12-25 10:59:05 +0300

    remove old usage

--- core/modules/system/tests/modules/ajax_forms_test/src/Form/AjaxFormsTestValidationForm.php
+++ core/modules/system/tests/modules/ajax_forms_test/src/Form/AjaxFormsTestValidationForm.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Drupal\ajax_forms_test\Form;
 
+use Drupal\ajax_forms_test\Callbacks;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 
@@ -25,13 +26,14 @@ class AjaxFormsTestValidationForm extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+    $callbacks = new Callbacks();
     $form['driver_text'] = [
       '#title' => $this->t('AJAX-enabled textfield.'),
       '#description' => $this->t("When this one AJAX-triggers and the spare required field is empty, you should not get an error."),
       '#type' => 'textfield',
       '#default_value' => $form_state->getValue('driver_text', ''),
       '#ajax' => [
-        'callback' => 'ajax_forms_test_validation_form_callback',
+        'callback' => [$callbacks, 'validationFormCallback'],
         'wrapper' => 'message_area',
         'method' => 'replaceWith',
       ],
@@ -44,7 +46,7 @@ class AjaxFormsTestValidationForm extends FormBase {
       '#type' => 'number',
       '#default_value' => $form_state->getValue('driver_number', ''),
       '#ajax' => [
-        'callback' => 'ajax_forms_test_validation_number_form_callback',
+        'callback' => [$callbacks, 'validationNumberFormCallback'],
         'wrapper' => 'message_area_number',
         'method' => 'replaceWith',
       ],
