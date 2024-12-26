92e3574624d63a089901dfe394037735bd7cec46
nlighteneddesign <nic@nlighteneddevelopment.com>
2024-12-25 22:22:42 -0500

    Better naming

--- core/lib/Drupal/Core/Extension/ModuleHandler.php
+++ core/lib/Drupal/Core/Extension/ModuleHandler.php
@@ -303,8 +303,8 @@ class ModuleHandler implements ModuleHandlerInterface {
    */
   public function getModuleImplementations(string $hook, string $module): array {
     $implementations = $this->getHookListeners($hook);
-    $implementations = $implementations[$module] ?? [];
-    return $implementations;
+    $module_implementations = $implementations[$module] ?? [];
+    return $module_implementations;
   }
 
   /**
