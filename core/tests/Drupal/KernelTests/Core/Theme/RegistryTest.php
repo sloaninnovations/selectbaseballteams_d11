d0427290a46ec67c436ba82204713c73766cd7d3
nicxvan <nic@nlightened.net>
2024-12-27 20:55:44 -0500

    Fix registry test kernel

--- core/tests/Drupal/KernelTests/Core/Theme/RegistryTest.php
+++ core/tests/Drupal/KernelTests/Core/Theme/RegistryTest.php
@@ -86,7 +86,10 @@ class RegistryTest extends KernelTestBase {
 
     $preprocess_functions = $registry_subsub_theme->get()['theme_test_template_test']['preprocess functions'];
     $this->assertSame([
-      'template_preprocess',
+      [
+        'module' => 'template',
+        'hook' => 'preprocess',
+      ],
       'test_base_theme_preprocess_theme_test_template_test',
       'test_subtheme_preprocess_theme_test_template_test',
       'test_subsubtheme_preprocess_theme_test_template_test',
@@ -94,14 +97,20 @@ class RegistryTest extends KernelTestBase {
 
     $preprocess_functions = $registry_sub_theme->get()['theme_test_template_test']['preprocess functions'];
     $this->assertSame([
-      'template_preprocess',
+      [
+        'module' => 'template',
+        'hook' => 'preprocess',
+      ],
       'test_base_theme_preprocess_theme_test_template_test',
       'test_subtheme_preprocess_theme_test_template_test',
     ], $preprocess_functions);
 
     $preprocess_functions = $registry_base_theme->get()['theme_test_template_test']['preprocess functions'];
     $this->assertSame([
-      'template_preprocess',
+      [
+        'module' => 'template',
+        'hook' => 'preprocess',
+      ],
       'test_base_theme_preprocess_theme_test_template_test',
     ], $preprocess_functions);
   }
@@ -120,8 +129,14 @@ class RegistryTest extends KernelTestBase {
 
     $suggestions = ['__kitten', '__flamingo'];
     $expected_preprocess_functions = [
-      'template_preprocess',
-      '\theme_test_preprocess_theme_test_preprocess_suggestions',
+      [
+        'module' => 'template',
+        'hook' => 'preprocess',
+      ],
+      [
+        'module' => 'theme_test',
+        'hook' => 'preprocess_theme_test_preprocess_suggestions',
+      ],
     ];
     $suggestion = '';
     $hook = 'theme_test_preprocess_suggestions';
@@ -133,8 +148,14 @@ class RegistryTest extends KernelTestBase {
     } while ($suggestion = array_shift($suggestions));
 
     $expected_preprocess_functions = [
-      'template_preprocess',
-      '\theme_test_preprocess_theme_test_preprocess_suggestions',
+      [
+        'module' => 'template',
+        'hook' => 'preprocess',
+      ],
+      [
+        'module' => 'theme_test',
+        'hook' => 'preprocess_theme_test_preprocess_suggestions',
+      ],
       'test_theme_preprocess_theme_test_preprocess_suggestions',
       'test_theme_preprocess_theme_test_preprocess_suggestions__kitten',
     ];
@@ -270,9 +291,18 @@ class RegistryTest extends KernelTestBase {
     $registry_theme->setThemeManager(\Drupal::theme());
 
     $expected = [
-      'template_preprocess',
-      'template_preprocess_container',
-      'template_preprocess_theme_test_registered_by_module',
+      [
+        'module' => 'template',
+        'hook' => 'preprocess',
+      ],
+      [
+        'module' => 'template',
+        'hook' => 'preprocess_container',
+      ],
+      [
+        'module' => 'template',
+        'hook' => 'preprocess_theme_test_registered_by_module',
+      ],
     ];
     $registry = $registry_theme->get();
     $this->assertEquals($expected, array_values($registry['theme_test_registered_by_module']['preprocess functions']));
