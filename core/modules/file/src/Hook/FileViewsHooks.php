1c4617b6923df62a14d825b9150d7b541ff93b26
nicxvan <nic@nlightened.net>
2024-12-29 09:49:09 -0500

    Call new service

--- core/modules/file/src/Hook/FileViewsHooks.php
+++ core/modules/file/src/Hook/FileViewsHooks.php
@@ -58,7 +58,7 @@ class FileViewsHooks {
     $pseudo_field_name = 'reverse_' . $field_name . '_' . $entity_type_id;
     /** @var \Drupal\Core\Entity\Sql\DefaultTableMapping $table_mapping */
     $table_mapping = $this->entityTypeManager->getStorage($entity_type_id)->getTableMapping();
-    [$label] = views_entity_field_label($entity_type_id, $field_name);
+    [$label] = \Drupal::service('entity_field.manager')->getFieldLabels($entity_type_id, $field_name);
     $data['file_managed'][$pseudo_field_name]['relationship'] = [
       'title' => t('@entity using @field', [
         '@entity' => $entity_type->getLabel(),
