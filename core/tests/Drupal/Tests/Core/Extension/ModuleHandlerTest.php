dbe54ac0eefcee249f61272d6a873f71f4101e1e
nlighteneddesign <nic@nlighteneddevelopment.com>
2025-01-01 10:40:45 -0500

    One more legacy

--- core/tests/Drupal/Tests/Core/Extension/ModuleHandlerTest.php
+++ core/tests/Drupal/Tests/Core/Extension/ModuleHandlerTest.php
@@ -79,8 +79,11 @@ class ModuleHandlerTest extends UnitTestCase {
    * Tests loading all modules.
    *
    * @covers ::loadAll
+   *
+   * @group legacy
    */
   public function testLoadAllModules(): void {
+    $this->expectDeprecation('module_handler_test_all1_module_implements_alter without a #[LegacyHook] attribute is deprecated in drupal:11.2.0 and removed in drupal:12.0.0. See https://www.drupal.org/node/3496788');
     $module_handler = $this->getModuleHandler();
     $module_handler->addModule('module_handler_test_all1', 'core/tests/Drupal/Tests/Core/Extension/modules/module_handler_test_all1');
     $module_handler->addModule('module_handler_test_all2', 'core/tests/Drupal/Tests/Core/Extension/modules/module_handler_test_all2');
