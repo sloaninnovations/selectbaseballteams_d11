648c5d9585ed11f6434680b2ea1f8f2c7b47458f
Nikolay Shapovalov <zniki.ru@1877422.no-reply.drupal.org>
2025-01-03 09:39:43 +0300

    Revert "add return, convert to static, refactor call"
    This reverts commit d29a89ee2a4c16af119cb294366076ef8d1bc636.

--- core/modules/system/tests/modules/ajax_forms_test/src/Callbacks.php
+++ core/modules/system/tests/modules/ajax_forms_test/src/Callbacks.php
@@ -31,7 +31,7 @@ class Callbacks {
   /**
    * Ajax callback triggered by select.
    */
-  public static function selectCallback($form, FormStateInterface $form_state): AjaxResponse {
+  public function selectCallback($form, FormStateInterface $form_state) {
     $response = new AjaxResponse();
     $response->addCommand(new HtmlCommand('#ajax_selected_color', $form_state->getValue('select')));
     $response->addCommand(new DataCommand('#ajax_selected_color', 'form_state_value_select', $form_state->getValue('select')));
@@ -41,7 +41,7 @@ class Callbacks {
   /**
    * Ajax callback triggered by date.
    */
-  public static function dateCallback($form, FormStateInterface $form_state): AjaxResponse {
+  public function dateCallback($form, FormStateInterface $form_state) {
     $response = new AjaxResponse();
     $date = $form_state->getValue('date');
     $response->addCommand(new HtmlCommand('#ajax_date_value', sprintf('<div>%s</div>', $date)));
@@ -52,7 +52,7 @@ class Callbacks {
   /**
    * Ajax callback triggered by datetime.
    */
-  public static function datetimeCallback($form, FormStateInterface $form_state): AjaxResponse {
+  public function datetimeCallback($form, FormStateInterface $form_state) {
     $datetime = $form_state->getValue('datetime')['date'] . ' ' . $form_state->getValue('datetime')['time'];
 
     $response = new AjaxResponse();
@@ -64,7 +64,7 @@ class Callbacks {
   /**
    * Ajax callback triggered by checkbox.
    */
-  public static function checkboxCallback($form, FormStateInterface $form_state): AjaxResponse {
+  public function checkboxCallback($form, FormStateInterface $form_state) {
     $response = new AjaxResponse();
     $response->addCommand(new HtmlCommand('#ajax_checkbox_value', $form_state->getValue('checkbox') ? 'checked' : 'unchecked'));
     $response->addCommand(new DataCommand('#ajax_checkbox_value', 'form_state_value_select', (int) $form_state->getValue('checkbox')));
@@ -74,7 +74,7 @@ class Callbacks {
   /**
    * Ajax callback to confirm image button was submitted.
    */
-  public static function imageButtonCallback($form, FormStateInterface $form_state): AjaxResponse {
+  public function imageButtonCallback($form, FormStateInterface $form_state) {
     $response = new AjaxResponse();
     $response->addCommand(new HtmlCommand('#ajax_image_button_result', "<div id='ajax-1-more-div'>Something witty!</div>"));
     return $response;
@@ -83,14 +83,14 @@ class Callbacks {
   /**
    * Ajax callback triggered by the checkbox in a #group.
    */
-  public static function checkboxGroupCallback($form, FormStateInterface $form_state): array {
+  public function checkboxGroupCallback($form, FormStateInterface $form_state) {
     return $form['checkbox_in_group_wrapper'];
   }
 
   /**
    * Ajax form callback: Selects 'after'.
    */
-  public static function advancedCommandsAfterCallback($form, FormStateInterface $form_state): AjaxResponse {
+  public function advancedCommandsAfterCallback($form, FormStateInterface $form_state): AjaxResponse {
     $selector = '#after_div';
 
     $response = new AjaxResponse();
@@ -101,7 +101,7 @@ class Callbacks {
   /**
    * Ajax form callback: Selects 'alert'.
    */
-  public static function advancedCommandsAlertCallback($form, FormStateInterface $form_state): AjaxResponse {
+  public function advancedCommandsAlertCallback($form, FormStateInterface $form_state): AjaxResponse {
     $response = new AjaxResponse();
     $response->addCommand(new AlertCommand('Alert'));
     return $response;
@@ -110,28 +110,28 @@ class Callbacks {
   /**
    * Ajax form callback: Selects 'announce' with no priority specified.
    */
-  public static function advancedCommandsAnnounceCallback($form, FormStateInterface $form_state): AjaxResponse {
+  public function advancedCommandsAnnounceCallback($form, FormStateInterface $form_state): AjaxResponse {
     return (new AjaxResponse())->addCommand(new AnnounceCommand('Default announcement.'));
   }
 
   /**
    * Ajax form callback: Selects 'announce' with 'polite' priority.
    */
-  public static function advancedCommandsAnnouncePoliteCallback($form, FormStateInterface $form_state): AjaxResponse {
+  public function advancedCommandsAnnouncePoliteCallback($form, FormStateInterface $form_state): AjaxResponse {
     return (new AjaxResponse())->addCommand(new AnnounceCommand('Polite announcement.', 'polite'));
   }
 
   /**
    * Ajax form callback: Selects 'announce' with 'assertive' priority.
    */
-  public static function advancedCommandsAnnounceAssertiveCallback($form, FormStateInterface $form_state): AjaxResponse {
+  public function advancedCommandsAnnounceAssertiveCallback($form, FormStateInterface $form_state): AjaxResponse {
     return (new AjaxResponse())->addCommand(new AnnounceCommand('Assertive announcement.', 'assertive'));
   }
 
   /**
    * Ajax form callback: Selects 'announce' with two announce commands returned.
    */
-  public static function advancedCommandsDoubleAnnounceCallback($form, FormStateInterface $form_state): AjaxResponse {
+  public function advancedCommandsDoubleAnnounceCallback($form, FormStateInterface $form_state): AjaxResponse {
     return (new AjaxResponse())->addCommand(new AnnounceCommand('Assertive announcement.', 'assertive'))
       ->addCommand(new AnnounceCommand('Another announcement.'));
   }
@@ -139,7 +139,7 @@ class Callbacks {
   /**
    * Ajax form callback: Selects 'append'.
    */
-  public static function advancedCommandsAppendCallback($form, FormStateInterface $form_state): AjaxResponse {
+  public function advancedCommandsAppendCallback($form, FormStateInterface $form_state): AjaxResponse {
     $selector = '#append_div';
     $response = new AjaxResponse();
     $response->addCommand(new AppendCommand($selector, "Appended text"));
@@ -149,7 +149,7 @@ class Callbacks {
   /**
    * Ajax form callback: Selects 'before'.
    */
-  public static function advancedCommandsBeforeCallback($form, FormStateInterface $form_state): AjaxResponse {
+  public function advancedCommandsBeforeCallback($form, FormStateInterface $form_state): AjaxResponse {
     $selector = '#before_div';
     $response = new AjaxResponse();
     $response->addCommand(new BeforeCommand($selector, "Before text"));
@@ -159,7 +159,7 @@ class Callbacks {
   /**
    * Ajax form callback: Selects 'changed'.
    */
-  public static function advancedCommandsChangedCallback($form, FormStateInterface $form_state): AjaxResponse {
+  public function advancedCommandsChangedCallback($form, FormStateInterface $form_state): AjaxResponse {
     $response = new AjaxResponse();
     $response->addCommand(new ChangedCommand('#changed_div'));
     return $response;
@@ -168,7 +168,7 @@ class Callbacks {
   /**
    * Ajax form callback: Selects 'changed' with asterisk marking inner div.
    */
-  public static function advancedCommandsChangedAsteriskCallback($form, FormStateInterface $form_state): AjaxResponse {
+  public function advancedCommandsChangedAsteriskCallback($form, FormStateInterface $form_state): AjaxResponse {
     $response = new AjaxResponse();
     $response->addCommand(new ChangedCommand('#changed_div', '#changed_div_mark_this'));
     return $response;
@@ -177,7 +177,7 @@ class Callbacks {
   /**
    * Ajax form callback: Selects 'css'.
    */
-  public static function advancedCommandsCssCallback($form, FormStateInterface $form_state): AjaxResponse {
+  public function advancedCommandsCssCallback($form, FormStateInterface $form_state): AjaxResponse {
     $selector = '#css_div';
     $color = 'blue';
 
@@ -189,7 +189,7 @@ class Callbacks {
   /**
    * Ajax form callback: Selects 'data'.
    */
-  public static function advancedCommandsDataCallback($form, FormStateInterface $form_state): AjaxResponse {
+  public function advancedCommandsDataCallback($form, FormStateInterface $form_state): AjaxResponse {
     $selector = '#data_div';
     $response = new AjaxResponse();
     $response->addCommand(new DataCommand($selector, 'test_key', 'test_value'));
@@ -199,7 +199,7 @@ class Callbacks {
   /**
    * Ajax form callback: Selects 'invoke'.
    */
-  public static function advancedCommandsInvokeCallback($form, FormStateInterface $form_state): AjaxResponse {
+  public function advancedCommandsInvokeCallback($form, FormStateInterface $form_state): AjaxResponse {
     $response = new AjaxResponse();
     $response->addCommand(new InvokeCommand('#invoke_div', 'addClass', ['error']));
     return $response;
@@ -208,7 +208,7 @@ class Callbacks {
   /**
    * Ajax form callback: Selects 'html'.
    */
-  public static function advancedCommandsHtmlCallback($form, FormStateInterface $form_state): AjaxResponse {
+  public function advancedCommandsHtmlCallback($form, FormStateInterface $form_state): AjaxResponse {
     $response = new AjaxResponse();
     $response->addCommand(new HtmlCommand('#html_div', 'replacement text'));
     return $response;
@@ -217,7 +217,7 @@ class Callbacks {
   /**
    * Ajax form callback: Selects 'insert'.
    */
-  public static function advancedCommandsInsertCallback($form, FormStateInterface $form_state): AjaxResponse {
+  public function advancedCommandsInsertCallback($form, FormStateInterface $form_state): AjaxResponse {
     $response = new AjaxResponse();
     $response->addCommand(new InsertCommand('#insert_div', 'insert replacement text'));
     return $response;
@@ -226,7 +226,7 @@ class Callbacks {
   /**
    * Ajax form callback: Selects 'prepend'.
    */
-  public static function advancedCommandsPrependCallback($form, FormStateInterface $form_state): AjaxResponse {
+  public function advancedCommandsPrependCallback($form, FormStateInterface $form_state): AjaxResponse {
     $response = new AjaxResponse();
     $response->addCommand(new PrependCommand('#prepend_div', "prepended text"));
     return $response;
@@ -235,7 +235,7 @@ class Callbacks {
   /**
    * Ajax form callback: Selects 'remove'.
    */
-  public static function advancedCommandsRemoveCallback($form, FormStateInterface $form_state): AjaxResponse {
+  public function advancedCommandsRemoveCallback($form, FormStateInterface $form_state): AjaxResponse {
     $response = new AjaxResponse();
     $response->addCommand(new RemoveCommand('#remove_text'));
     return $response;
@@ -244,7 +244,7 @@ class Callbacks {
   /**
    * Ajax form callback: Selects 'restripe'.
    */
-  public static function advancedCommandsRestripeCallback($form, FormStateInterface $form_state): AjaxResponse {
+  public function advancedCommandsRestripeCallback($form, FormStateInterface $form_state): AjaxResponse {
     $response = new AjaxResponse();
     $response->addCommand(new RestripeCommand('#restripe_table'));
     return $response;
@@ -253,7 +253,7 @@ class Callbacks {
   /**
    * Ajax form callback: Selects 'settings'.
    */
-  public static function advancedCommandsSettingsCallback($form, FormStateInterface $form_state): AjaxResponse {
+  public function advancedCommandsSettingsCallback($form, FormStateInterface $form_state): AjaxResponse {
     $setting['ajax_forms_test']['foo'] = 42;
     $response = new AjaxResponse();
     $response->addCommand(new SettingsCommand($setting));
@@ -263,7 +263,7 @@ class Callbacks {
   /**
    * Ajax callback for 'add_css'.
    */
-  public static function advancedCommandsAddCssCallback($form, FormStateInterface $form_state): AjaxResponse {
+  public function advancedCommandsAddCssCallback($form, FormStateInterface $form_state): AjaxResponse {
     $response = new AjaxResponse();
     $response->addCommand(new AddCssCommand([
       [
@@ -281,7 +281,7 @@ class Callbacks {
   /**
    * Ajax form callback: Selects the 'driver_text' element of the validation form.
    */
-  public static function validationFormCallback($form, FormStateInterface $form_state): array {
+  public function validationFormCallback($form, FormStateInterface $form_state): array {
     \Drupal::messenger()->addStatus("ajax_forms_test_validation_form_callback invoked");
     \Drupal::messenger()->addStatus(t("Callback: driver_text=%driver_text, spare_required_field=%spare_required_field", ['%driver_text' => $form_state->getValue('driver_text'), '%spare_required_field' => $form_state->getValue('spare_required_field')]));
     return ['#markup' => '<div id="message_area">ajax_forms_test_validation_form_callback at ' . date('c') . '</div>'];
@@ -290,7 +290,7 @@ class Callbacks {
   /**
    * Ajax form callback: Selects the 'driver_number' element of the validation form.
    */
-  public static function validationNumberFormCallback($form, FormStateInterface $form_state): array {
+  public function validationNumberFormCallback($form, FormStateInterface $form_state): array {
     \Drupal::messenger()->addStatus("ajax_forms_test_validation_number_form_callback invoked");
     \Drupal::messenger()->addStatus(t("Callback: driver_number=%driver_number, spare_required_field=%spare_required_field", ['%driver_number' => $form_state->getValue('driver_number'), '%spare_required_field' => $form_state->getValue('spare_required_field')]));
     return ['#markup' => '<div id="message_area_number">ajax_forms_test_validation_number_form_callback at ' . date('c') . '</div>'];
@@ -299,7 +299,7 @@ class Callbacks {
   /**
    * AJAX form callback: Selects for the ajax_forms_test_lazy_load_form() form.
    */
-  public static function lazyLoadFormAjax($form, FormStateInterface $form_state): array {
+  public function lazyLoadFormAjax($form, FormStateInterface $form_state): array {
     $build = [
       '#markup' => 'new content',
     ];
