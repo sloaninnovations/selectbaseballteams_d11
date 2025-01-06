77dfe0058c2839d903bc1ac6c6e4983489020880
nicxvan <nic@nlightened.net>
2025-01-05 22:56:37 -0500

    Fix test

--- core/modules/system/tests/modules/batch_test/src/Form/BatchTestSimpleForm.php
+++ core/modules/system/tests/modules/batch_test/src/Form/BatchTestSimpleForm.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Drupal\batch_test\Form;
 
+use Drupal\batch_test\BatchTestDefinitions;
 use Drupal\batch_test\BatchTestHelper;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
@@ -53,11 +54,12 @@ class BatchTestSimpleForm extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+    $batch_test_definitions = new BatchTestDefinitions();
     $batch_test_helper = new BatchTestHelper();
     $batch_test_helper->stack(NULL, TRUE);
 
     foreach ($form_state->getValue('batch') as $batch) {
-      batch_set($batch_test_helper->$batch());
+      batch_set($batch_test_definitions->$batch());
     }
 
     $form_state->setRedirect('batch_test.redirect');
