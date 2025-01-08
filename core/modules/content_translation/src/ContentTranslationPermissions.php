95ee6fce62f76eef0fbe068b13fa5f117ed1a0ef
Alex Pott <alex.a.pott@googlemail.com>
2025-01-08 10:20:32 +0000

    Fix permissions when content translation status changes

--- core/modules/content_translation/src/ContentTranslationPermissions.php
+++ core/modules/content_translation/src/ContentTranslationPermissions.php
@@ -79,17 +79,23 @@ class ContentTranslationPermissions implements ContainerInjectionInterface {
           case 'bundle':
             foreach ($this->entityTypeBundleInfo->getBundleInfo($entity_type_id) as $bundle => $bundle_info) {
               if ($this->contentTranslationManager->isEnabled($entity_type_id, $bundle)) {
-                $permissions["translate $bundle $entity_type_id"] = $this->buildBundlePermission($entity_type, $bundle, $bundle_info);
+                $permissions[static::permissionKey($entity_type, $bundle)] = $this->buildBundlePermission($entity_type, $bundle, $bundle_info);
               }
             }
             break;
 
           case 'entity_type':
             if ($this->contentTranslationManager->isEnabled($entity_type_id)) {
-              $permissions["translate $entity_type_id"] = [
+              $permissions[static::permissionKey($entity_type)] = [
                 'title' => $this->t('Translate @entity_label', ['@entity_label' => $entity_type->getSingularLabel()]),
                 'dependencies' => ['module' => [$entity_type->getProvider()]],
               ];
+              // @see \Drupal\content_translation\ContentTranslationManager::isEnabled()
+              $bundles = array_keys($this->entityTypeBundleInfo->getBundleInfo($entity_type_id));
+              foreach ($bundles as $bundle) {
+                $config = $this->entityTypeManager->getStorage('language_content_settings')->load($entity_type->id() . '.' . $bundle);
+                $permission['dependencies'][$config->getConfigDependencyKey()][] = $config->getConfigDependencyName();
+              }
             }
             break;
         }
@@ -97,6 +103,25 @@ class ContentTranslationPermissions implements ContainerInjectionInterface {
     }
 
     return $permissions;
+  }
+
+  /**
+   * Gets the permission key for an entity type and bundle.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeInterface $entity_type
+   *   The entity type to get the permission for.
+   * @param string|null $bundle
+   *   The bundle to get the permission for.
+   *
+   * @return string|null
+   *   The permission name.
+   */
+  public static function permissionKey(EntityTypeInterface $entity_type, ?string $bundle = NULL): ?string {
+    return match ($entity_type->getPermissionGranularity()) {
+      'bundle' => "translate $bundle " . $entity_type->id(),
+      'entity_type' => "translate " . $entity_type->id(),
+      default => NULL,
+    };
   }
 
   /**
@@ -128,6 +153,9 @@ class ContentTranslationPermissions implements ContainerInjectionInterface {
     else {
       $permission['dependencies']['module'][] = $entity_type->getProvider();
     }
+
+    $config = $this->entityTypeManager->getStorage('language_content_settings')->load($entity_type->id() . '.' . $bundle);
+    $permission['dependencies'][$config->getConfigDependencyKey()][] = $config->getConfigDependencyName();
     return $permission;
   }
 
