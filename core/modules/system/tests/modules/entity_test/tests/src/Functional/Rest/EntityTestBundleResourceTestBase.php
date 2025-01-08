2a02b028d52e4f3c68675c8af078df56c42ecacf
quietone <quietone@2572884.no-reply.drupal.org>
2025-01-09 10:16:14 +1300

    test modules

--- core/modules/system/tests/modules/entity_test/tests/src/Functional/Rest/EntityTestBundleResourceTestBase.php
+++ core/modules/system/tests/modules/entity_test/tests/src/Functional/Rest/EntityTestBundleResourceTestBase.php
@@ -7,6 +7,9 @@ namespace Drupal\Tests\entity_test\Functional\Rest;
 use Drupal\entity_test\Entity\EntityTestBundle;
 use Drupal\Tests\rest\Functional\EntityResource\ConfigEntityResourceTestBase;
 
+/**
+ * Resource test base for the entity_test entity.
+ */
 abstract class EntityTestBundleResourceTestBase extends ConfigEntityResourceTestBase {
 
   /**
