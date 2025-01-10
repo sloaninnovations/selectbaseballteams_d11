2b13011f74b0ad81bad771d1c4250501ee2be77c
Matthew Oliveira <m4olivei@gmail.com>
2025-01-10 11:01:17 -0500

    Issue #3441576: Eslint / Prettier pass of user-block.js plus early return to reduce Christmas tree

--- core/modules/navigation/js/user-block.js
+++ core/modules/navigation/js/user-block.js
@@ -1,12 +1,13 @@
 /**
  * @file
  *
- * Customizes the user block to use the current username without affecting to cache.
+ * Customizes the user block to use the current username without affecting
+ * caching.
  */
 
 ((Drupal, drupalSettings, once) => {
   /**
-   * Replaces the generic User with the actual username.
+   * Replaces the generic 'My Account' text with the actual username.
    *
    * @type {Drupal~behavior}
    *
@@ -15,19 +16,18 @@
    */
   Drupal.behaviors.navigationUsername = {
     attach: (context, settings) => {
-      if (settings?.navigation?.user) {
-        once('user-block', '[data-user-block]', context).forEach(
-          (userBlock) => {
-            userBlock
-              .querySelectorAll(
-                '.toolbar-button--icon--navigation-user-links-user-wrapper [data-toolbar-text], .toolbar-popover__header [data-toolbar-text]',
-              )
-              .forEach((button) => {
-                button.textContent = settings.navigation.user;
-              });
-          },
-        );
+      if (!settings?.navigation?.user) {
+        return;
       }
+      once('user-block', '[data-user-block]', context).forEach((userBlock) =>
+        userBlock
+          .querySelectorAll(
+            '.toolbar-button--icon--navigation-user-links-user-wrapper [data-toolbar-text], .toolbar-popover__header [data-toolbar-text]',
+          )
+          .forEach((button) => {
+            button.textContent = settings.navigation.user;
+          }),
+      );
     },
   };
 })(Drupal, drupalSettings, once);
