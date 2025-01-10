abecde1513b9ddb2b7f402eb31b95fbb83f4b753
Matthew Oliveira <m4olivei@gmail.com>
2025-01-10 11:13:49 -0500

    Issue #3441576: Also update data-icon-text and data-index-test for the user menu item according to user name

--- core/modules/navigation/js/user-block.js
+++ core/modules/navigation/js/user-block.js
@@ -19,15 +19,20 @@
       if (!settings?.navigation?.user) {
         return;
       }
-      once('user-block', '[data-user-block]', context).forEach((userBlock) =>
+      once('user-block', '[data-user-block]', context).forEach((userBlock) => {
         userBlock
           .querySelectorAll(
-            '.toolbar-button--icon--navigation-user-links-user-wrapper [data-toolbar-text], .toolbar-popover__header [data-toolbar-text]',
+            '.toolbar-button--icon--navigation-user-links-user-wrapper, .toolbar-popover__header',
           )
           .forEach((button) => {
-            button.textContent = settings.navigation.user;
-          }),
-      );
+            const buttonLabel = button.querySelector('[data-toolbar-text]');
+            button.dataset.indexText = settings.navigation.user.charAt(0);
+            button.dataset.iconText = settings.navigation.user.substring(0, 2);
+            if (buttonLabel) {
+              buttonLabel.textContent = settings.navigation.user;
+            }
+          });
+      });
     },
   };
 })(Drupal, drupalSettings, once);
