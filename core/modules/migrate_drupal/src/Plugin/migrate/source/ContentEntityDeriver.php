ae0b33518660c4ec24442f90579102c158fb4f05
Benji Fisher <benji@FisherFam.org>
2025-01-11 00:24:49 -0500

    Move the deriver class to the migrate module

--- core/modules/migrate_drupal/src/Plugin/migrate/source/ContentEntityDeriver.php
+++ core/modules/migrate_drupal/src/Plugin/migrate/source/ContentEntityDeriver.php
@@ -2,23 +2,17 @@
 
 namespace Drupal\migrate_drupal\Plugin\migrate\source;
 
-use Drupal\Component\Plugin\Derivative\DeriverBase;
-use Drupal\Core\Entity\ContentEntityTypeInterface;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\Plugin\Discovery\ContainerDeriverInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\migrate\Plugin\migrate\source\ContentEntityDeriver as MigrateContentEntityDeriver;
 
 /**
  * Deriver for content entity source plugins.
+ *
+ * @deprecated in drupal:11.2.0 and is removed from drupal:12.0.0. Use
+ * \Drupal\migrate\Plugin\migrate\source\ContentEntityDeriver instead.
+ *
+ * @see https://www.drupal.org/node/3498916
  */
-class ContentEntityDeriver extends DeriverBase implements ContainerDeriverInterface {
-
-  /**
-   * The entity type manager.
-   *
-   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
-   */
-  protected $entityTypeManager;
+class ContentEntityDeriver extends MigrateContentEntityDeriver {
 
   /**
    * Constructs a new ContentEntityDeriver.
@@ -29,32 +23,8 @@ class ContentEntityDeriver extends DeriverBase implements ContainerDeriverInterf
    *   The entity type manager.
    */
   public function __construct($base_plugin_id, EntityTypeManagerInterface $entityTypeManager) {
-    $this->entityTypeManager = $entityTypeManager;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, $base_plugin_id) {
-    return new static(
-      $base_plugin_id,
-      $container->get('entity_type.manager')
-    );
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getDerivativeDefinitions($base_plugin_definition) {
-    $this->derivatives = [];
-    foreach ($this->entityTypeManager->getDefinitions() as $id => $definition) {
-      if ($definition instanceof ContentEntityTypeInterface) {
-        $this->derivatives[$id] = $base_plugin_definition;
-        // Provide entity_type so the source can be used apart from a deriver.
-        $this->derivatives[$id]['entity_type'] = $id;
-      }
-    }
-    return parent::getDerivativeDefinitions($base_plugin_definition);
+    @trigger_error(__CLASS__ . ' is deprecated in drupal:11.2.0 and is removed from drupal:12.0.0. Use \Drupal\migrate\Plugin\migrate\source\ContentEntity instead. See https://www.drupal.org/node/3498916', E_USER_DEPRECATED);
+    parent::__construct($base_plugin_id, $entityTypeManager);
   }
 
 }
