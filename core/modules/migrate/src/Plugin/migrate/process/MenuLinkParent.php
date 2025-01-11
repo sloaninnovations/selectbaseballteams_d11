358d75f63358853a85b16fd10d24f662063c2e68
Benji Fisher <benji@FisherFam.org>
2025-01-11 14:47:13 -0500

    Add the lookup_migrations option to the menu_link_parent process plugin

--- core/modules/migrate/src/Plugin/migrate/process/MenuLinkParent.php
+++ core/modules/migrate/src/Plugin/migrate/process/MenuLinkParent.php
@@ -36,7 +36,12 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * - parent_link_path: The Drupal path or external URL the parent of this menu
  *   link points to.
  *
- * Example:
+ * There is one configuration option:
+ * - lookup_migrations: (optional) An array of migration IDs. If missing or
+ *   null, then the current migration is used to look up parent_id. If provided,
+ *   then use just the listed migrations for that look-up.
+ *
+ * Examples:
  *
  * @code
  * process:
@@ -51,6 +56,35 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * in the source (e.g., '20'). If that fails, try to determine the parent by a
  * combination of a menu name (e.g., 'management') and a parent menu link path
  * (e.g., 'admin/structure').
+ *
+ * @code
+ * process:
+ *   parent:
+ *     plugin: menu_link_parent
+ *     source:
+ *       - plid
+ *       - menu_name
+ *       - parent_link_path
+ *     lookup_migrations: []
+ * @endcode
+ * In this example, skip the migration lookup and find the parent item using
+ * just the menu name and the link path.
+ *
+ * @code
+ * process:
+ *   parent:
+ *     plugin: menu_link_parent
+ *     source:
+ *       - plid
+ *       - menu_name
+ *       - parent_link_path
+ *     lookup_migrations:
+ *       - this_migration
+ *       - another_migration
+ * @endcode
+ * In this example, use this_migration and another_migration (in that order) to
+ * look up plid. When lookup_migrations is provided, the current migration is
+ * not added by default. If you want to use it, then include it in the list.
  *
  * @see https://www.drupal.org/docs/8/api/menu-api
  * @see \Drupal\migrate\Plugin\MigrateProcessInterface
@@ -111,6 +145,11 @@ class MenuLinkParent extends ProcessPluginBase implements ContainerFactoryPlugin
     $this->migrateLookup = $migrate_lookup;
     $this->menuLinkManager = $menu_link_manager;
     $this->menuLinkStorage = $menu_link_storage;
+
+    $lookup_migrations = $this->configuration['lookup_migrations'] ?? NULL;
+    if ($lookup_migrations !== NULL && !is_array($lookup_migrations)) {
+      throw new \InvalidArgumentException('If provided, "lookup_migrations" must be an array or null.');
+    }
   }
 
   /**
@@ -141,9 +180,13 @@ class MenuLinkParent extends ProcessPluginBase implements ContainerFactoryPlugin
       return '';
     }
 
-    $lookup_result = $this->migrateLookup->lookup($this->migration->id(), [$parent_id]);
-    if ($lookup_result) {
-      $already_migrated_id = $lookup_result[0]['id'];
+    $lookup_migrations = $this->configuration['lookup_migrations'] ?? [$this->migration->id()];
+    foreach ($lookup_migrations as $migration_id) {
+      $lookup_result = $this->migrateLookup->lookup($migration_id, [$parent_id]);
+      if ($lookup_result) {
+        $already_migrated_id = $lookup_result[0]['id'];
+        break;
+      }
     }
 
     if (!empty($already_migrated_id) && ($link = $this->menuLinkStorage->load($already_migrated_id))) {
