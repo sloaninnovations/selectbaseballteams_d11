60120e012295b2d6996a1b5f72b08db55a2e216d
catch <6915-catch@users.noreply.drupalcode.org>
2025-01-11 09:52:49 +0000

    One less cache set.

--- core/profiles/standard/tests/src/FunctionalJavascript/StandardPerformanceTest.php
+++ core/profiles/standard/tests/src/FunctionalJavascript/StandardPerformanceTest.php
@@ -129,7 +129,7 @@ class StandardPerformanceTest extends PerformanceTestBase {
     $this->assertSame($expected_queries, $recorded_queries);
     $this->assertSame(36, $performance_data->getQueryCount());
     $this->assertSame(123, $performance_data->getCacheGetCount());
-    $this->assertSame(45, $performance_data->getCacheSetCount());
+    $this->assertSame(44, $performance_data->getCacheSetCount());
     $this->assertSame(0, $performance_data->getCacheDeleteCount());
     $this->assertSame(37, $performance_data->getCacheTagChecksumCount());
     $this->assertSame(43, $performance_data->getCacheTagIsValidCount());
