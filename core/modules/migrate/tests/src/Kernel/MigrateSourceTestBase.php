fcb4112d4436415db5a87638078c2bbd97f0b74e
mondrake <28163-mondrake@users.noreply.drupalcode.org>
2025-01-14 09:59:38 +0000

    Update file MigrateSourceTestBase.php

--- core/modules/migrate/tests/src/Kernel/MigrateSourceTestBase.php
+++ core/modules/migrate/tests/src/Kernel/MigrateSourceTestBase.php
@@ -208,6 +208,9 @@ abstract class MigrateSourceTestBase extends KernelTestBase {
 
   /**
    * Provides source data for ::testSource.
+   *
+   * @return iterable
+   *   The source data.
    */
   abstract public static function providerSource();
 
