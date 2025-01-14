6f841c0fc3675476353e2f2d0eb980322fb60e0c
Alex Pott <alex.a.pott@googlemail.com>
2025-01-14 09:55:29 +0000

    Revert "Make test fail"
    This reverts commit 8d140d49041d1ea62ddb30da0465187538f3ecf2.

--- core/tests/Drupal/Tests/Core/Access/AccessManagerTest.php
+++ core/tests/Drupal/Tests/Core/Access/AccessManagerTest.php
@@ -133,7 +133,6 @@ class AccessManagerTest extends UnitTestCase {
    * Tests \Drupal\Core\Access\AccessManager::setChecks().
    */
   public function testSetChecks(): void {
-    $this->assertFalse(TRUE);
     // Check setChecks without any access checker defined yet.
     $this->checkProvider->setChecks($this->routeCollection);
 
