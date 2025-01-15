f5f9aecd4c19aa3a70afe7b5fa3dbf9a70695212
Sascha Grossenbacher <saschagros@gmail.com>
2025-01-16 00:10:19 +0100

    update registry tests

--- core/tests/Drupal/KernelTests/Core/Theme/RegistryTest.php
+++ core/tests/Drupal/KernelTests/Core/Theme/RegistryTest.php
@@ -83,7 +83,6 @@ class RegistryTest extends KernelTestBase {
 
     $preprocess_functions = $registry_subsub_theme->get()['theme_test_template_test']['preprocess functions'];
     $this->assertSame([
-      'template_preprocess',
       'test_base_theme_preprocess_theme_test_template_test',
       'test_subtheme_preprocess_theme_test_template_test',
       'test_subsubtheme_preprocess_theme_test_template_test',
@@ -91,14 +90,12 @@ class RegistryTest extends KernelTestBase {
 
     $preprocess_functions = $registry_sub_theme->get()['theme_test_template_test']['preprocess functions'];
     $this->assertSame([
-      'template_preprocess',
       'test_base_theme_preprocess_theme_test_template_test',
       'test_subtheme_preprocess_theme_test_template_test',
     ], $preprocess_functions);
 
     $preprocess_functions = $registry_base_theme->get()['theme_test_template_test']['preprocess functions'];
     $this->assertSame([
-      'template_preprocess',
       'test_base_theme_preprocess_theme_test_template_test',
     ], $preprocess_functions);
   }
@@ -117,7 +114,6 @@ class RegistryTest extends KernelTestBase {
 
     $suggestions = ['__kitten', '__flamingo'];
     $expected_preprocess_functions = [
-      'template_preprocess',
       'theme_test_preprocess_theme_test_preprocess_suggestions',
     ];
     $suggestion = '';
@@ -130,7 +126,6 @@ class RegistryTest extends KernelTestBase {
     } while ($suggestion = array_shift($suggestions));
 
     $expected_preprocess_functions = [
-      'template_preprocess',
       'theme_test_preprocess_theme_test_preprocess_suggestions',
       'test_theme_preprocess_theme_test_preprocess_suggestions',
       'test_theme_preprocess_theme_test_preprocess_suggestions__kitten',
@@ -267,7 +262,6 @@ class RegistryTest extends KernelTestBase {
     $registry_theme->setThemeManager(\Drupal::theme());
 
     $expected = [
-      'template_preprocess',
       'template_preprocess_container',
       'template_preprocess_theme_test_registered_by_module',
     ];
