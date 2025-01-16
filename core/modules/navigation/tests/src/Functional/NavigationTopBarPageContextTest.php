8a7fa69576c2208cee0c0acf1f3be0ca258b0020
Pablo López // plopesc <plopesc@gmail.com>
2025-01-16 15:37:33 +0100

    Issue #3484600: Refactor test

--- core/modules/navigation/tests/src/Functional/NavigationTopBarPageContextTest.php
+++ core/modules/navigation/tests/src/Functional/NavigationTopBarPageContextTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Drupal\Tests\navigation\Functional;
 
+use Drupal\Core\Url;
 use Drupal\Tests\BrowserTestBase;
 use Drupal\Tests\node\Traits\ContentTypeCreationTrait;
 use Drupal\Tests\node\Traits\NodeCreationTrait;
@@ -26,7 +27,7 @@ class NavigationTopBarPageContextTest extends BrowserTestBase {
     'node',
     'navigation',
     'navigation_top_bar',
-    'block',
+    'test_page_test',
   ];
 
   /**
@@ -49,8 +50,6 @@ class NavigationTopBarPageContextTest extends BrowserTestBase {
 
     // Create and log in an administrative user.
     $this->adminUser = $this->drupalCreateUser([
-      'administer site configuration',
-      'access administration pages',
       'access navigation',
       'bypass node access',
     ]);
@@ -63,17 +62,22 @@ class NavigationTopBarPageContextTest extends BrowserTestBase {
   /**
    * Tests the PageContext top bar item output for a published node.
    */
-  public function testPageContextPublishedNode(): void {
+  public function testPageContextTopBarItemNode(): void {
     // Create a published node entity.
-    $published_node = $this->createNode([
+    $node = $this->createNode([
       'type' => 'article',
       'title' => 'No easy twist on the bow',
       'status' => 1,
       'uid' => $this->adminUser->id(),
     ]);
 
+    $test_page_url = Url::fromRoute('test_page_test.test_page');
+    $this->drupalGet($test_page_url);
+    // Ensure the top bar item is not present.
+    $this->assertSession()->elementNotExists('css', '.navigation-top-bar-context');
+
     // Test the PageContext output for the published node.
-    $this->drupalGet($published_node->toUrl());
+    $this->drupalGet($node->toUrl());
     $this->assertSession()->statusCodeEquals(200);
     // Ensure the top bar exists
     $this->assertSession()->elementExists('css', '.navigation-top-bar-context');
@@ -81,27 +85,15 @@ class NavigationTopBarPageContextTest extends BrowserTestBase {
     $this->assertSession()->pageTextContains('No easy twist on the bow');
     // Check the CSS class for published status
     $this->assertSession()->elementContains('css', '.context-status.published', 'Published');
-  }
 
-  /**
-   * Tests the PageContext top bar item output for an unpublished node.
-   */
-  public function testPageContextUnpublishedNode(): void {
-    // Create an unpublished node entity.
-    $unpublished_node = $this->createNode([
-      'type' => 'article',
-      'title' => 'Precision and agility in navigating challenges',
-      'status' => 0,
-      'uid' => $this->adminUser->id(),
-    ]);
+    // Unpublish the node.
+    $node->setUnpublished();
+    $node->save();
 
     // Test the PageContext output for the unpublished node.
-    $this->drupalGet($unpublished_node->toUrl());
-    $this->assertSession()->statusCodeEquals(200);
-    // Ensure the top bar exists
-    $this->assertSession()->elementExists('css', '.navigation-top-bar-context');
+    $this->drupalGet($node->toUrl());
     // Check the node title
-    $this->assertSession()->pageTextContains('Precision and agility in navigating challenges');
+    $this->assertSession()->pageTextContains('No easy twist on the bow');
     // Check the CSS class for unpublished status
     $this->assertSession()->elementContains('css', '.context-status.unpublished', 'Unpublished');
   }
