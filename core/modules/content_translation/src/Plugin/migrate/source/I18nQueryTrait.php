00f74cafe1232712ead498d9ec948a5ad2550020
Edward Wu <godotislate@gmail.com>
2025-01-16 18:10:15 -0800

    Add deprecation message.

--- core/modules/content_translation/src/Plugin/migrate/source/I18nQueryTrait.php
+++ core/modules/content_translation/src/Plugin/migrate/source/I18nQueryTrait.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\content_translation\Plugin\migrate\source;
 
+@trigger_error('The ' . __NAMESPACE__ . '\I18nQueryTrait is deprecated in drupal:11.2.0 and is removed from drupal:12.0.0. Use \Drupal\migrate_drupal\Plugin\migrate\source\I18nQueryTrait instead. See https://www.drupal.org/node/3439256', E_USER_DEPRECATED);
+
 use Drupal\migrate_drupal\Plugin\migrate\source\I18nQueryTrait as MigrateDrupalI18nQueryTrait;
 
 /**
