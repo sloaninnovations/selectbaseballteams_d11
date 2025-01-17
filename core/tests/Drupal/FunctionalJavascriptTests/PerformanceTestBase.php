217cb62a9a6d1067db6e5a6bcd572db775b738fb
Pablo López // plopesc <plopesc@gmail.com>
2025-01-17 12:57:21 +0100

    Issue #3500489: Performance tests need to run cron

--- core/tests/Drupal/FunctionalJavascriptTests/PerformanceTestBase.php
+++ core/tests/Drupal/FunctionalJavascriptTests/PerformanceTestBase.php
@@ -27,6 +27,7 @@ class PerformanceTestBase extends WebDriverTestBase {
   protected function setUp(): void {
     parent::setUp();
     $this->doSetUpTasks();
+    \Drupal::service('cron')->run();
   }
 
   /**
