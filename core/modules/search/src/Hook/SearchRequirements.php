c7eafe9732f7652d60725881294ce109dd6ba4a9
nicxvan <nic@nlightened.net>
2025-01-17 21:54:20 -0500

    convert search_requirements

---
+++ core/modules/search/src/Hook/SearchRequirements.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\search\Hook;
+
+use Drupal\Core\Hook\Attribute\Hook;
+use Drupal\search\SearchPageRepositoryInterface;
+
+/**
+ * Requirements for the search module.
+ */
+class SearchRequirements {
+
+  public function __construct(
+    protected readonly SearchPageRepositoryInterface $searchPageRepository,
+  ) {}
+
+  /**
+   * Implements hook_runtime_requirements().
+   *
+   * For the Status Report, return information about search index status.
+   */
+  #[Hook('runtime_requirements')]
+  public function runtime(): array {
+    $requirements = [];
+    $remaining = 0;
+    $total = 0;
+    foreach ($this->searchPageRepository->getIndexableSearchPages() as $entity) {
+      $status = $entity->getPlugin()->indexStatus();
+      $remaining += $status['remaining'];
+      $total += $status['total'];
+    }
+
+    $done = $total - $remaining;
+    // Use floor() to calculate the percentage, so if it is not quite 100%, it
+    // will show as 99%, to indicate "almost done".
+    $percent = ($total > 0 ? floor(100 * $done / $total) : 100);
+    $requirements['search_status'] = [
+      'title' => t('Search index progress'),
+      'value' => t('@percent% (@remaining remaining)', ['@percent' => $percent, '@remaining' => $remaining]),
+      'severity' => REQUIREMENT_INFO,
+    ];
+    return $requirements;
+  }
+
+}
