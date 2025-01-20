2a6fce95669562e32bed2f0f9c563d5871d758f4
Sascha Grossenbacher <5019-berdir@users.noreply.drupalcode.org>
2025-01-20 11:54:56 +0000

    Update LanguageAddForm.php

--- core/modules/language/src/Form/LanguageAddForm.php
+++ core/modules/language/src/Form/LanguageAddForm.php
@@ -155,6 +155,7 @@ class LanguageAddForm extends LanguageFormBase {
       // Translate the label to the current language, this is consistent with
       // the label in the form select, see
       // \Drupal\language\ConfigurableLanguageManager::getStandardLanguageListWithoutConfigured().
+      // phpcs:ignore Drupal.Semantics.FunctionT.NotLiteralString
       $label = $this->t($standard_languages[$langcode][0]);
       $direction = $standard_languages[$langcode][2] ?? ConfigurableLanguage::DIRECTION_LTR;
     }
