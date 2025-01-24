19aa82e82e12d61eb48e102a4f29772446bc0a19
quietone <quietone@2572884.no-reply.drupal.org>
2025-01-24 23:42:59 +1300

    remove t()

--- core/modules/navigation/tests/src/Functional/NavigationLogoTest.php
+++ core/modules/navigation/tests/src/Functional/NavigationLogoTest.php
@@ -91,7 +91,7 @@ class NavigationLogoTest extends BrowserTestBase {
       'logo_provider' => 'custom',
       'logo_path' => $logo_file->getFileUri(),
     ];
-    $this->submitForm($edit, t('Save configuration'));
+    $this->submitForm($edit, 'Save configuration');
     // Refresh the page to verify custom logo is placed.
     $this->drupalGet('/admin/config/user-interface/navigation/settings');
     $this->assertSession()->elementExists('css', 'a.admin-toolbar__logo > img');
@@ -102,7 +102,7 @@ class NavigationLogoTest extends BrowserTestBase {
       'logo_provider' => 'custom',
       'logo_path' => 'core/misc/logo/drupal-logo.svg',
     ];
-    $this->submitForm($edit, t('Save configuration'));
+    $this->submitForm($edit, 'Save configuration');
     // Refresh the page to verify custom logo is placed.
     $this->drupalGet('/admin/config/user-interface/navigation/settings');
     $this->assertSession()->elementExists('css', 'a.admin-toolbar__logo > img');
@@ -114,7 +114,7 @@ class NavigationLogoTest extends BrowserTestBase {
       'logo_provider' => 'custom',
       'files[logo_upload]' => $this->fileSystem->realpath($file->uri),
     ];
-    $this->submitForm($edit, t('Save configuration'));
+    $this->submitForm($edit, 'Save configuration');
     $this->assertSession()->statusMessageContains('The image was resized to fit within the navigation logo expected dimensions of 40x40 pixels. The new dimensions of the resized image are 40x27 pixels.');
     // Refresh the page to verify custom logo is placed.
     $this->drupalGet('/admin/config/user-interface/navigation/settings');
