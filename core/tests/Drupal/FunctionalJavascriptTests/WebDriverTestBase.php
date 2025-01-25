3c9940e73543d66634c46be7d2f074ac5aedd877
Sascha Grossenbacher <saschagros@gmail.com>
2025-01-25 12:07:20 +0100

    phpcs

--- core/tests/Drupal/FunctionalJavascriptTests/WebDriverTestBase.php
+++ core/tests/Drupal/FunctionalJavascriptTests/WebDriverTestBase.php
@@ -57,20 +57,7 @@ abstract class WebDriverTestBase extends BrowserTestBase {
     }
     $this->minkDefaultDriverArgs = ['chrome', ['goog:chromeOptions' => ['w3c' => FALSE]], 'http://localhost:4444'];
 
-    //try {
-      return parent::initMink();
-    /*}
-    catch (DriverException $e) {
-      if ($this->minkDefaultDriverClass === DrupalSelenium2Driver::class) {
-        $this->markTestSkipped("The test wasn't able to connect to your webdriver instance. For more information read core/tests/README.md.\n\nThe original message while starting Mink: {$e->getMessage()}");
-      }
-      else {
-        throw $e;
-      }
-    }
-    catch (\Exception $e) {
-      $this->markTestSkipped('An unexpected error occurred while starting Mink: ' . $e->getMessage());
-    }*/
+    return parent::initMink();
   }
 
   /**
